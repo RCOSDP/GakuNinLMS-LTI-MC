816a01ed8cf967b485c2cffe4e2eaa7660bdb10c
Kohei Watanabe <kou029w@users.noreply.github.com>
2020-05-29 16:21:30 +0900

    refact: tableコンポーネントの整理

--- components/ContentsTable.tsx
+++ components/ContentsTable.tsx
@@ -1,16 +1,9 @@
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
 import Edit from "@material-ui/icons/Edit";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
 import LibraryAdd from "@material-ui/icons/LibraryAdd";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import MaterialTable, { Column } from "material-table";
 import { useRouter } from "components/hooks";
 import { isArray } from "util";
+import { Table } from "./Table";
+import { Column } from "material-table";
 
 type ContentsRow = {
   id: string;
@@ -18,50 +11,21 @@ type ContentsRow = {
   editable: boolean;
 };
 
-type ContentsTableRows = Array<ContentsRow>;
-
-const column = {
-  index: { title: "#", field: "index", width: "4rem" },
-  contentName: {
-    title: "名称",
-    field: "name",
-  },
-};
-
-const contentsTableTitle = "学習コンテンツ一覧";
-
-function withIndex<T>(data: T[]) {
-  return data.map((e, index) => ({ ...e, index: index + 1 /* one-based */ }));
-}
-
-export function ContentsTable(props: { data: ContentsTableRows }) {
+export function ContentsTable(props: { data: ContentsRow[] }) {
   const router = useRouter();
 
-  const title = contentsTableTitle;
-  const columns: Column<ContentsRow>[] = [column.index, column.contentName].map(
-    (c) => ({
-      ...c,
-      headerStyle: { padding: 0 },
-      cellStyle: {
-        padding: 0,
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
-      },
-    })
-  );
-
   return (
-    <MaterialTable
-      title={title}
-      columns={columns}
-      localization={{
-        header: {
-          actions: "",
-        },
-      }}
-      components={{ Container: (props) => <div {...props} /> }}
-      data={withIndex(props.data)}
+    <Table
+      title="学習コンテンツ一覧"
+      columns={
+        [
+          { title: "#", field: "index", width: "4rem" },
+          {
+            title: "名称",
+            field: "name",
+          },
+        ] as Column<ContentsRow>[]
+      }
       actions={[
         (row) => ({
           icon: Edit,
@@ -86,31 +50,7 @@ export function ContentsTable(props: { data: ContentsTableRows }) {
             }),
         },
       ]}
-      options={{
-        sorting: true,
-        tableLayout: "fixed",
-        rowStyle: { height: 49 },
-        pageSize: 50,
-        pageSizeOptions: ((opts, rows) => {
-          const i = opts.findIndex((n) => rows <= n);
-          return opts.slice(0, i >= 0 ? i + 1 : opts.length);
-        })([50, 100], props.data.length),
-        actionsColumnIndex: -1,
-        actionsCellStyle: { padding: 0 },
-      }}
-      icons={
-        {
-          Clear,
-          FirstPage,
-          LastPage,
-          NextPage: ChevronRight,
-          PreviousPage: ChevronLeft,
-          ResetSearch: Clear,
-          Search,
-          SortArrow: ArrowDownward,
-          Export: SaveAlt,
-        } as any
-      }
+      data={props.data}
     />
   );
 }
--- components/VideosTable.tsx
+++ components/VideosTable.tsx
@@ -1,73 +1,36 @@
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
 import Edit from "@material-ui/icons/Edit";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
 import LibraryAdd from "@material-ui/icons/LibraryAdd";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import MaterialTable, { Column } from "material-table";
 import { useRouter } from "components/hooks";
 import { isArray } from "util";
+import { Table } from "./Table";
+import { Column } from "material-table";
 
-type Row = {
+type VideosRow = {
   id: string;
   name: string;
-  description: string;
   editable: boolean;
 };
 
-type Table = Array<Row>;
-
-const column = {
-  index: { title: "#", field: "index", width: "4rem" },
-  name: {
-    title: "名称",
-    field: "name",
-    width: "20%",
-  },
-  description: {
-    title: "概要",
-    field: "description",
-  },
-};
-
-function withIndex<T>(data: T[]) {
-  return data.map((e, index) => ({ ...e, index: index + 1 /* one-based */ }));
-}
-
-export function VideosTable(props: { data: Table }) {
+export function VideosTable(props: { data: VideosRow[] }) {
   const router = useRouter();
 
-  const title = "ビデオ一覧";
-  const columns: Column<Row>[] = [
-    column.index,
-    column.name,
-    column.description,
-  ].map((c) => ({
-    ...c,
-    headerStyle: { padding: 0 },
-    cellStyle: {
-      padding: 0,
-      whiteSpace: "nowrap",
-      overflow: "hidden",
-      textOverflow: "ellipsis",
-    },
-  }));
-
   return (
-    <MaterialTable
-      title={title}
-      columns={columns}
-      localization={{
-        header: {
-          actions: "",
-        },
-      }}
-      components={{ Container: (props) => <div {...props} /> }}
-      data={withIndex(props.data)}
+    <Table
+      title="ビデオ一覧"
+      columns={
+        [
+          { title: "#", field: "index", width: "4rem" },
+          {
+            title: "名称",
+            field: "name",
+            width: "20%",
+          },
+          {
+            title: "概要",
+            field: "description",
+          },
+        ] as Column<VideosRow>[]
+      }
       actions={[
         (row) => ({
           icon: Edit,
@@ -92,31 +55,7 @@ export function VideosTable(props: { data: Table }) {
             }),
         },
       ]}
-      options={{
-        sorting: true,
-        tableLayout: "fixed",
-        rowStyle: { height: 49 },
-        pageSize: 50,
-        pageSizeOptions: ((opts, rows) => {
-          const i = opts.findIndex((n) => rows <= n);
-          return opts.slice(0, i >= 0 ? i + 1 : opts.length);
-        })([50, 100], props.data.length),
-        actionsColumnIndex: -1,
-        actionsCellStyle: { padding: 0 },
-      }}
-      icons={
-        {
-          Clear,
-          FirstPage,
-          LastPage,
-          NextPage: ChevronRight,
-          PreviousPage: ChevronLeft,
-          ResetSearch: Clear,
-          Search,
-          SortArrow: ArrowDownward,
-          Export: SaveAlt,
-        } as any
-      }
+      data={props.data}
     />
   );
 }
