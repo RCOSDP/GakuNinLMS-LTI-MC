d6912be5c2cfda256019a6120b179b0f38d41929
Kohei Watanabe <kou029w@users.noreply.github.com>
2020-06-01 19:35:18 +0900

    format

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 const path = process.env.NEXT_PUBLIC_API_BASE_PATH || "";
 export default function () {
   useEffect(() => {
-    if (typeof window === 'undefined') return;
-    const url = new URL(path, document.location.href).href
+    if (typeof window === "undefined") return;
+    const url = new URL(path, document.location.href).href;
     if (document.location.href !== url) {
       document.location.replace(url);
     }
