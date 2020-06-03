4fdb7fc8406f19027cc862689b6119d35ebcffbb
Kohei Watanabe <kou029w@gmail.com>
2020-06-03 12:00:01 +0900

    create *Table.strories

---
+++ components/VideosTable.stories.tsx
@@ -0,0 +1,16 @@
+export default { title: "VideosTable" };
+import { VideosTable } from "./VideosTable";
+import { Videos } from "./video";
+
+const props: Videos = {
+  videos: [2, 4, 6, 8].map((id) => ({
+    id,
+    title: `Sample Video ${id}`,
+    description: `Sample Description ${id}`,
+  })),
+  state: "success",
+};
+
+export function Basic() {
+  return <VideosTable {...props} />;
+}
