1ebe66604429b1ff2052082e57033ea94ddc7546
Kimiaki Kuno <knokmki612@gmail.com>
2020-11-17 10:29:48 +0900

    docs: add stories of Button, Typography, Dialog

---
+++ components/atoms/Typography.stories.tsx
@@ -0,0 +1,40 @@
+export default { title: "atoms/Typography" };
+
+import Typography from "@material-ui/core/Typography";
+
+export const Default = () => (
+  <div>
+    <Typography variant="h1">h1</Typography>
+    <Typography variant="h2">h2</Typography>
+    <Typography variant="h3">h3</Typography>
+    <Typography variant="h4">h4</Typography>
+    <Typography variant="h5">h5</Typography>
+    <Typography variant="h6">h6</Typography>
+    <Typography variant="subtitle1">subtitle1</Typography>
+    <Typography variant="subtitle2">subtitle2</Typography>
+    <Typography variant="body1">body1</Typography>
+    <Typography variant="body2">body2</Typography>
+  </div>
+);
+
+export const Book = () => (
+  <div>
+    <Typography variant="h4" gutterBottom={true}>
+      ブック「コンピュータ・サイエンス」の編集
+    </Typography>
+    <Typography variant="h5" gutterBottom={true}>
+      トピック・セクションの編集
+    </Typography>
+  </div>
+);
+
+export const Topic = () => (
+  <div>
+    <Typography variant="subtitle1">
+      ブック「コンピュータ・サイエンス」内
+    </Typography>
+    <Typography variant="h4" gutterBottom={true}>
+      トピック「リンゴに夢中のレッサーパンダ」の編集
+    </Typography>
+  </div>
+);
---
+++ components/organisms/Dialog.stories.tsx
@@ -0,0 +1,43 @@
+export default { title: "organisms/Dialog" };
+
+import { forwardRef, useState } from "react";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import Slide from "@material-ui/core/Slide";
+import CloseIcon from "@material-ui/icons/Close";
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export const Default = () => {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        ダイアログ
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <DialogContent>
+          <IconButton color="inherit" onClick={handleClose} aria-label="close">
+            <CloseIcon />
+          </IconButton>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
