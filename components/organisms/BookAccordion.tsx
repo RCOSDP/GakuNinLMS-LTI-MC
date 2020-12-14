49c5b2d7a4f7f08fbd393b876a603607cb03674d
Kimiaki Kuno <knokmki612@gmail.com>
2020-12-14 11:04:14 +0900

    refactor: use same type in components

--- components/organisms/BookAccordion.tsx
+++ components/organisms/BookAccordion.tsx
@@ -13,7 +13,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import BookAccordionChildren from "$molecules/BookAccordionChildren";
 import CourseChip from "$atoms/CourseChip";
 import Item from "$atoms/Item";
-import { Book } from "types/books";
+import { Book } from "types/book";
 import useAccordionStyle from "styles/accordion";
 import useAccordionSummaryStyle from "styles/accordionSummary";
 import useAccordionDetailStyle from "styles/accordionDetail";
