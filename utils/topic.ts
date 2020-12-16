7a33047f5ce006c41899b42363d699849e553443
Kohei Watanabe <kou029w@gmail.com>
2020-12-16 15:37:00 +0900

    feat: {create,update,destroy}Topic

--- utils/topic.ts
+++ utils/topic.ts
@@ -1,6 +1,6 @@
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import { api } from "./api";
-import { TopicSchema } from "$server/models/topic";
+import { TopicProps, TopicSchema } from "$server/models/topic";
 
 const key = "/api/v2/topic/{topic_id}";
 
@@ -12,4 +12,23 @@ async function fetchTopic(_: typeof key, id: TopicSchema["id"]) {
 export function useTopic(id: TopicSchema["id"]) {
   const { data } = useSWR<TopicSchema>([key, id], fetchTopic);
   return data;
+}
+
+export async function createTopic(body: TopicProps): Promise<TopicSchema> {
+  const res = await api.apiV2TopicPost({ body });
+  await mutate([key, res.id], res);
+  return res as TopicSchema;
+}
+
+export async function updateTopic({
+  id,
+  ...body
+}: TopicProps & { id: TopicSchema["id"] }): Promise<TopicSchema> {
+  const res = await api.apiV2TopicTopicIdPut({ topicId: id, body });
+  await mutate([key, res.id], res);
+  return res as TopicSchema;
+}
+
+export async function destroyTopic(id: TopicSchema["id"]) {
+  await api.apiV2TopicTopicIdDelete({ topicId: id });
 }
