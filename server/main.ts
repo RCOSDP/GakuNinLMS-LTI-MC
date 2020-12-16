6bbaa60e8f7044c0adf43fe98a3ba7233a7871ef
Kohei Watanabe <kou029w@gmail.com>
2020-12-16 19:05:47 +0900

    feat: support production

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,12 @@
 import fastify from "fastify";
+import fastifyStatic from "fastify-static";
+import path from "path";
 import {
   PORT,
   BASE_PATH,
   SESSION_SECRET,
   FRONTEND_ORIGIN,
+  FRONTEND_PATH,
 } from "$server/utils/env";
 import { sessionStore } from "$server/utils/prisma";
 import app, { Options } from "$server/config/app";
@@ -11,6 +14,11 @@ import app, { Options } from "$server/config/app";
 const isDev = process.env.NODE_ENV !== "production";
 
 fastify({ logger: isDev })
+  .register(fastifyStatic, {
+    root: path.resolve(__dirname, "static"),
+    prefix: FRONTEND_PATH,
+    extensions: ["html"],
+  })
   .register(app, {
     basePath: BASE_PATH,
     allowOrigin: [FRONTEND_ORIGIN],
