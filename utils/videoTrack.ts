4335b87c888b5b2465ba076e0bc715c7c60f2024
Kohei Watanabe <kou029w@gmail.com>
2020-12-23 15:53:35 +0900

    feat: 字幕ファイルアップロード/削除クライアントAPIの提供

---
+++ utils/videoTrack.ts
@@ -0,0 +1,32 @@
+import { Resource, Track } from "@prisma/client";
+import { VideoTrackProps, VideoTrackSchema } from "$server/models/videoTrack";
+import { api } from "./api";
+
+export async function uploadVideoTrack(
+  resourceId: Resource["id"],
+  props: Omit<VideoTrackProps, "content">,
+  content: Blob | string
+) {
+  const contentText =
+    typeof content === "string" ? content : await content.text();
+
+  const uploaded = await api.apiV2ResourceResourceIdVideoTrackPost({
+    resourceId,
+    body: {
+      ...props,
+      content: contentText,
+    },
+  });
+
+  return uploaded as VideoTrackSchema;
+}
+
+export async function destroyVideoTrack(
+  resourceId: Resource["id"],
+  videoTrackId: Track["id"]
+) {
+  await api.apiV2ResourceResourceIdVideoTrackVideoTrackIdDelete({
+    resourceId,
+    videoTrackId,
+  });
+}
