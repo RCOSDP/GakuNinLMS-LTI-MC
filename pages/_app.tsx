e320bb24ddac2f86efecaa8d2bfd11cedafc52a5
Kohei Watanabe <kou029w@gmail.com>
2021-01-06 16:08:36 +0900

    chore: install ConfirmProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+import type { ReactNode } from "react";
 import { Provider } from "jotai";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { ThemeProvider } from "@material-ui/core/styles";
+import MuiThemeProvider from "@material-ui/styles/ThemeProvider";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { ConfirmProvider } from "material-ui-confirm";
 import theme from "$theme";
 import AppBar from "$organisms/AppBar";
 // NOTE: For VideoJs components.
@@ -10,18 +12,30 @@ import "video.js/dist/video-js.css";
 import "videojs-seek-buttons/dist/videojs-seek-buttons.css";
 import "$styles/video-js.css";
 
-function App({ Component, pageProps }: AppProps) {
+function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-    <Provider>
+    <>
       <Head>
         <title>ChibiCHiLO</title>
         <meta name="viewport" content="width=device-width" />
         <meta name="theme-color" content={theme.palette.primary.main} />
       </Head>
-      <ThemeProvider theme={theme}>
+      <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar position="sticky" />
-        <Component {...pageProps} />
+        {children}
+      </MuiThemeProvider>
+    </>
+  );
+}
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider>
+      <ThemeProvider>
+        <ConfirmProvider>
+          <Component {...pageProps} />
+        </ConfirmProvider>
       </ThemeProvider>
     </Provider>
   );
