fb10100a1de9e9397449c0da1d450871232114e5
Kohei Watanabe <kou029w@gmail.com>
2021-01-14 15:44:05 +0900

    feat: use Placeholder in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import { UrlObject } from "url";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import { isInstructor, useSession } from "$utils/session";
 import { LtiResourceLinkSchema } from "$server/models/ltiResourceLink";
 import Unknown from "$templates/Unknown";
+import Placeholder from "$templates/Placeholder";
 
 function Replace(props: { href: string | UrlObject }) {
   const router = useRouter();
   router.replace(props.href);
-  return <Link href={props.href}>Link</Link>; // TODO: プレースホルダーがいい加減
+  return <Placeholder />;
 }
 
 function ReplaceToBook(props: { ltiResourceLink: LtiResourceLinkSchema }) {
@@ -24,7 +24,7 @@ function Router() {
   const session = useSession();
 
   // TODO: https://github.com/npocccties/ChibiCHiLO/issues/3
-  if (!session.data) return <div>Loading...</div>; // TODO: プレースホルダーがいい加減
+  if (!session.data) return <Placeholder />;
   if (isInstructor(session.data)) return <Replace href="/books" />;
   if (!session.data.ltiResourceLink)
     return (
