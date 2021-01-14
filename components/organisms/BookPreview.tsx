e5809219f45a9129cdc08adf9af5c90463d463ef
Kohei Watanabe <kou029w@gmail.com>
2021-01-14 11:53:57 +0900

    fix: typo Blonk→Blank

--- components/organisms/BookPreview.tsx
+++ components/organisms/BookPreview.tsx
@@ -4,7 +4,7 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import CheckBoxOutlineBlonkIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined";
 import { InfoOutlined, EditOutlined } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
@@ -84,7 +84,7 @@ export default function BookPreview(props: Props) {
             color="primary"
             onClick={handleCheckBoxClick}
           >
-            {checkBox ? <CheckBoxOutlinedIcon /> : <CheckBoxOutlineBlonkIcon />}
+            {checkBox ? <CheckBoxOutlinedIcon /> : <CheckBoxOutlineBlankIcon />}
           </IconButton>
           {book.name}
           <IconButton onClick={handleInfoClick}>
