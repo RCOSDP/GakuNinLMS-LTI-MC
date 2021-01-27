3b5e6ed356d69cec94d83e57977ed1cb9da425dc
Kimiaki Kuno <knokmki612@gmail.com>
2021-01-27 16:48:54 +0900

    fix: import module mismatch

--- utils/session.ts
+++ utils/session.ts
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 import type { Session } from "$server/utils/session";
-import { InlineResponse2004ToJSON } from "$openapi/models";
+import { InlineResponse2005ToJSON } from "$openapi/models";
 import { api } from "./api";
 
 export * from "$server/utils/session";
@@ -8,6 +8,6 @@ export * from "$server/utils/session";
 export function useSession() {
   return useSWR<Session>("/api/v2/session", async () => {
     const body = await api.apiV2SessionGet();
-    return InlineResponse2004ToJSON(body);
+    return InlineResponse2005ToJSON(body);
   });
 }
