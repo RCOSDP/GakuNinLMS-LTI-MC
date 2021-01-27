6197c7ab663003557c9aac31e74be58e9daf239a
Kohei Watanabe <kou029w@gmail.com>
2021-01-27 23:49:28 +0900

    refactor: 自動生成される番号を使わない

--- utils/session.ts
+++ utils/session.ts
@@ -1,13 +1,12 @@
 import useSWR from "swr";
 import type { Session } from "$server/utils/session";
-import { InlineResponse2006ToJSON } from "$openapi/models";
 import { api } from "./api";
 
 export * from "$server/utils/session";
 
 export function useSession() {
   return useSWR<Session>("/api/v2/session", async () => {
-    const body = await api.apiV2SessionGet();
-    return InlineResponse2006ToJSON(body);
+    const res = await api.apiV2SessionGetRaw();
+    return res.raw.json();
   });
 }
