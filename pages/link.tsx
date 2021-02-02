fc7bb6d674394c81d328108290cd7296d5070f44
Kimiaki Kuno <knokmki612@gmail.com>
2021-02-02 14:27:25 +0900

    feat: ブック連携で自身以外の共有されているブックを列挙するように

--- pages/link.tsx
+++ pages/link.tsx
@@ -1,35 +1,43 @@
-import { User } from "@prisma/client";
 import { useRouter } from "next/router";
 import type { BookSchema } from "$server/models/book";
+import type { UserSchema } from "$server/models/user";
+import bookCreateBy from "$utils/bookCreateBy";
 import { useSession } from "$utils/session";
 import { updateLtiResourceLink } from "$utils/ltiResourceLink";
-import { useUserBooks } from "$utils/userBooks";
+import { useBooks } from "$utils/books";
 import BookLink from "$templates/BookLink";
 import Unknown from "$templates/Unknown";
 import Placeholder from "$templates/Placeholder";
+import { createBook } from "$utils/book";
 import { pagesPath } from "$utils/$path";
 
-function Link(
-  props: Omit<Parameters<typeof BookLink>[0], "books"> & { userId: User["id"] }
-) {
-  const userBooks = useUserBooks(props.userId);
-  const books = userBooks.data?.books ?? [];
-  return <BookLink {...props} books={books} />;
-}
+const sharedOrCreatedBy = (author?: Pick<UserSchema, "id">) => (
+  book: BookSchema
+) => {
+  return book.shared || bookCreateBy(book, author);
+};
 
-function Index() {
+function Link() {
   const router = useRouter();
-  const session = useSession();
-  const userId = session.data?.user?.id;
-  const ltiLaunchBody = session.data?.ltiLaunchBody;
+  const { data: session, error } = useSession();
+  const books = useBooks();
+  const ltiLaunchBody = session?.ltiLaunchBody;
   const ltiResourceLink = ltiLaunchBody && {
     id: ltiLaunchBody.resource_link_id,
     title: ltiLaunchBody.resource_link_title ?? "",
     contextId: ltiLaunchBody.context_id,
     contextTitle: ltiLaunchBody.context_title ?? "",
   };
-  async function handleSubmit(bookId: BookSchema["id"]) {
+  async function handleSubmit(book: BookSchema) {
     if (ltiResourceLink == null) return;
+    const connectOrCreateBook = async (book: BookSchema) => {
+      if (!session?.user) return Promise.reject();
+      if (bookCreateBy(book, session.user)) return Promise.resolve(book.id);
+      // NOTE: 自身以外の作成したブックに関しては影響を及ぼすのを避ける目的で複製
+      const { ltiResourceLinks: _, ...bookProps } = book;
+      return createBook(bookProps).then(({ id }) => id);
+    };
+    const bookId = await connectOrCreateBook(book);
     await updateLtiResourceLink({ ...ltiResourceLink, bookId });
     return router.push(pagesPath.book.$url({ query: { bookId } }));
   }
@@ -49,15 +57,23 @@ function Index() {
     onBookNewClick: handleBookNew,
   };
 
-  if (userId == null) return <Placeholder />;
   if (ltiResourceLink == null) return <Placeholder />;
-  if (session.error) {
+  if (!books) return <Placeholder />;
+  if (error) {
     return <Unknown header="セッション情報が得られませんでした" />;
   }
 
   return (
-    <Link userId={userId} ltiResourceLink={ltiResourceLink} {...handlers} />
+    <BookLink
+      books={books.filter(sharedOrCreatedBy(session?.user))}
+      ltiResourceLink={ltiResourceLink}
+      {...handlers}
+    />
   );
 }
 
-export default Index;
+function Router() {
+  return <Link />;
+}
+
+export default Router;
