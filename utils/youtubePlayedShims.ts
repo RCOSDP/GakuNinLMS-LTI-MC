dbc0c2aeb646b1a8eeccb4ba1a2bfe3b70f6726c
Kohei Watanabe <kou029w@gmail.com>
2021-02-12 01:43:01 +0900

    fix: 無効な時間範囲が含まれる問題の修正 - seeking中は無視 - timeupdateは通常 4-66Hz 程度なので playbackRate が 1.0 のとき currentTime の差分が 500 ms を超えているならば無効と判定 https://html.spec.whatwg.org/multipage/media.html#playing-the-media-resource

--- utils/youtubePlayedShims.ts
+++ utils/youtubePlayedShims.ts
@@ -3,6 +3,19 @@ import { IntervalTree } from "node-interval-tree";
 
 const youtubeType = "video/youtube";
 
+/**
+ * 無効な速度で再生しているか否か
+ * timeupdate イベント発火頻度は通常 4-66Hz 程度
+ * playbackRate が 1.0 のとき currentTime の差分が 500 ms を超えているならば無効と判定
+ * https://html.spec.whatwg.org/multipage/media.html#playing-the-media-resource
+ * @param player VideoJsPlayer
+ * @param diff timeupdate 発火時の currentTime の差分 (秒)
+ * @return 無効な速度で再生しているならば true、それ以外ならば false
+ */
+function invalidSpeed(player: VideoJsPlayer, diff: number) {
+  return Math.abs(diff) > 0.5 * player.playbackRate();
+}
+
 /** HTMLMediaElement.played に相当するAPIの再現 */
 function youtubePlayedShims(player: VideoJsPlayer) {
   if (player.currentType() !== youtubeType) return;
@@ -19,9 +32,17 @@ function youtubePlayedShims(player: VideoJsPlayer) {
   }
 
   let start = player.currentTime();
+  let prev = start;
   player.on("timeupdate", () => {
     const end = player.currentTime();
-    if (end < start) start = end;
+    const diff = end - prev;
+    prev = end;
+
+    if (end <= start || player.seeking() || invalidSpeed(player, diff)) {
+      start = end;
+      return;
+    }
+
     tree.search(start, end).forEach((range) => tree.remove(range));
     tree.insert({ low: start, high: end });
   });
