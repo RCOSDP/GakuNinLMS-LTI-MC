89b038ea73f53312e0d82a37a688456cafb98ce3
Kohei Watanabe <kou029w@gmail.com>
2021-03-02 13:18:46 +0900

    refactor: updateSessionAtom

--- utils/session.ts
+++ utils/session.ts
@@ -1,13 +1,8 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import useSWR, { mutate } from "swr";
-import type { TopicSchema } from "$server/models/topic";
-import type { BookSchema } from "$server/models/book";
 import type { SessionSchema } from "$server/models/session";
-import { isAdministrator, isInstructor } from "$server/utils/session";
 import { useUpdateSessionAtom } from "$store/session";
 import { api } from "./api";
-import topicCreateBy from "./topicCreateBy";
-import bookCreateBy from "./bookCreateBy";
 
 export * from "$server/utils/session";
 
@@ -18,29 +13,17 @@ export function useSessionInit() {
     const res = await api.apiV2SessionGetRaw();
     return res.raw.json();
   });
-  const sessionWithState = useMemo(
-    () => ({
-      session: data,
-      isAdministrator: data ? isAdministrator(data) : false,
-      isInstructor: data ? isInstructor(data) : false,
-      isTopicEditable(topic: Pick<TopicSchema, "creator">) {
-        // NOTE: 自身以外の作成したトピックに関しては編集不可
-        return topicCreateBy(topic, data?.user);
-      },
-      isBookEditable(book: Pick<BookSchema, "author">) {
-        // NOTE: 自身以外の作成したブックに関しては編集不可
-        return bookCreateBy(book, data?.user);
-      },
-      error: Boolean(error),
-    }),
-    [data, error]
-  );
-  const updateSession = useUpdateSessionAtom();
+  const [state, update] = useUpdateSessionAtom();
   useEffect(() => {
-    updateSession(sessionWithState);
-  }, [sessionWithState, updateSession]);
+    if (data) {
+      update({
+        session: data,
+        error: Boolean(error),
+      });
+    }
+  }, [data, error, update]);
 
-  return sessionWithState;
+  return state;
 }
 
 export function revalidateSession(): Promise<SessionSchema> {
