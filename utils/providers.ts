fab2d60cd5980a064c12b968841af7c0221ce6a2
Kohei Watanabe <kou029w@gmail.com>
2021-03-04 17:28:02 +0900

    feat: URLの自動補完

---
+++ utils/providers.ts
@@ -0,0 +1,19 @@
+import { NEXT_PUBLIC_API_BASE_PATH } from "./env";
+
+/** サポートしている動画配信サービス一覧 */
+export const providers = {
+  youtube: {
+    name: "YouTube",
+    baseUrl: "https://www.youtube.com/watch?v=",
+  },
+  vimeo: {
+    name: "Vimeo",
+    baseUrl: "https://vimeo.com/",
+  },
+  wowza: {
+    name: "Wowza",
+    baseUrl: new URL("/api/v2/wowza/", NEXT_PUBLIC_API_BASE_PATH).href,
+  },
+} as const;
+
+export default providers;
