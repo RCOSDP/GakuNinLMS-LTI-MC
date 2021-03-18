1be0000a08749d52a20b61cb415c5db7d9ec12fb
Kimiaki Kuno <knokmki612@gmail.com>
2021-03-18 12:17:46 +0900

    feat: onSortChangeイベント

--- components/atoms/SortSelect.stories.tsx
+++ components/atoms/SortSelect.stories.tsx
@@ -2,4 +2,4 @@ export default { title: "atoms/SortSelect" };
 
 import SortSelect from "./SortSelect";
 
-export const Default = () => <SortSelect />;
+export const Default = () => <SortSelect onSortChange={console.log} />;
