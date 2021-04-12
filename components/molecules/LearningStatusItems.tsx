d27a92e076d1c9257354aa3cd6aaa892a58f26bf
Kimiaki Kuno <knokmki612@gmail.com>
2021-04-12 19:16:48 +0900

    feat: 学習状況のラベルをクリックした際に表示する学習者リスト

--- components/molecules/LearningStatusItems.tsx
+++ components/molecules/LearningStatusItems.tsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 import LearningStatusDot from "$atoms/LearningStatusDot";
+import useSelectorProps from "$utils/useSelectorProps";
 import type { BookLearningActivitySchema } from "$server/models/bookLearningActivity";
 import type { TopicLearningActivitySchema } from "$server/models/topicLearningActivity";
 import type { LearnerActivitySchema } from "$server/models/learnerActivity";
@@ -17,7 +20,22 @@ const useStyles = makeStyles((theme) => ({
     "& > :first-child": {
       marginRight: theme.spacing(0.5),
     },
+    "&$clickable": {
+      textDecoration: "underline",
+    },
   },
+  button: {
+    appearance: "none",
+    border: "none",
+    background: "transparent",
+    padding: theme.spacing(0.5),
+    cursor: "pointer",
+    "&:disabled": {
+      color: "unset",
+      cursor: "default",
+    },
+  },
+  clickable: {},
 }));
 
 type Props = {
@@ -35,33 +53,73 @@ export default function LearningStatusItems(props: Props) {
     onLearnerActivityClick,
   } = props;
   const classes = useStyles();
+  const learnerActivitiesMenu = useSelectorProps<LearnerActivitySchema>(null);
+
+  const items = [
+    {
+      type: "completed",
+      label: "完了",
+      count: learningActivity?.completedCount,
+    },
+    {
+      type: "incompleted",
+      label: "未完了",
+      count: learningActivity?.incompletedCount,
+    },
+    {
+      type: "unopened",
+      label: "未開封",
+      count:
+        (learningActivity &&
+          learningActivity.totalLearnerCount -
+            learningActivity.completedCount -
+            learningActivity.incompletedCount) ||
+        0,
+    },
+  ] as const;
+  const clickable = learnerActivities && learnerActivities.length > 0;
+  const handleLearnerActivityClick = (
+    learnerActivity: LearnerActivitySchema
+  ) => () => {
+    onLearnerActivityClick?.(learnerActivity);
+    learnerActivitiesMenu.onSelect(learnerActivity);
+  };
 
   return (
     <div className={clsx(className, classes.root)}>
-      <div className={classes.item}>
-        <LearningStatusDot type="completed" />
-        <span>
-          完了{learningActivity && `${learningActivity.completedCount}人`}
-        </span>
-      </div>
-      <div className={classes.item}>
-        <LearningStatusDot type="incompleted" />
-        <span>
-          未完了{learningActivity && `${learningActivity.incompletedCount}人`}
-        </span>
-      </div>
-      <div className={classes.item}>
-        <LearningStatusDot type="unopened" />
-        <span>
-          未開封
-          {learningActivity &&
-            `${
-              learningActivity.totalLearnerCount -
-              learningActivity.completedCount -
-              learningActivity.incompletedCount
-            }人`}
-        </span>
-      </div>
+      {items.map((item, index) => (
+        <button
+          key={index}
+          className={clsx(classes.item, classes.button, {
+            [classes.clickable]: clickable,
+          })}
+          disabled={!clickable}
+          onClick={learnerActivitiesMenu.onOpen}
+        >
+          <LearningStatusDot type={item.type} />
+          <span>
+            {item.label}
+            {learningActivity && `${item.count}人`}
+          </span>
+        </button>
+      ))}
+      <Menu
+        id="learner-activities-menu"
+        aria-haspopup="true"
+        anchorEl={learnerActivitiesMenu.anchorEl}
+        open={Boolean(learnerActivitiesMenu.anchorEl)}
+        onClose={learnerActivitiesMenu.onClose}
+      >
+        {learnerActivities &&
+          learnerActivities.map((learnerActivity, index) => (
+            <MenuItem
+              key={index}
+              onClick={handleLearnerActivityClick(learnerActivity)}
+            >
+              {learnerActivity.name}
+            </MenuItem>
+          ))}
+      </Menu>
     </div>
   );
 }
