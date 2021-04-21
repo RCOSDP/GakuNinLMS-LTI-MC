b97e8adb4fb03ad90b60d5f4be1f77d723458fc0
Kohei Watanabe <kou029w@gmail.com>
2021-04-21 11:53:03 +0900

    fix: `typeof window`を使う最適化

--- components/organisms/EmbedProblem.tsx
+++ components/organisms/EmbedProblem.tsx
@@ -2,13 +2,13 @@ import Link from "@material-ui/core/Link";
 import Problem from "./Problem";
 
 export default function EmbedProblem() {
-  if (typeof document === "undefined") return null;
+  if (typeof window === "undefined") return null;
 
   return (
     <Problem title="このコンテンツは埋め込み表示できません">
       このコンテンツはフレーム内に埋め込み表示できません。新しいウィンドウで開いてください。
       <p>
-        <Link href={document.location.href} target="_blank">
+        <Link href={window.location.href} target="_blank">
           新しいウィンドウで開く
         </Link>
       </p>
