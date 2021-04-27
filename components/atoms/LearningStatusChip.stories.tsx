7d5db53233fd87eda974539e8b2fa0c67758bb28
Kimiaki Kuno <knokmki612@gmail.com>
2021-04-27 14:13:34 +0900

    fix: no-unused-vars

--- components/atoms/LearningStatusChip.stories.tsx
+++ components/atoms/LearningStatusChip.stories.tsx
@@ -1,14 +1,11 @@
 export default { title: "atoms/LearningStatusChip" };
 
-import type { LearningStatus } from "$server/models/learningStatus";
 import LearningStatusChip from "./LearningStatusChip";
 
 export const Default = () => (
   <>
-    {(["completed", "incompleted", "unopened"] as const).map(
-      (type, index) => (
-        <LearningStatusChip key={index} type={type} />
-      )
-    )}
+    {(["completed", "incompleted", "unopened"] as const).map((type, index) => (
+      <LearningStatusChip key={index} type={type} />
+    ))}
   </>
 );
