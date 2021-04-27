d5210894869c9985ac7ab0f0f7c301f7a5c56582
Kimiaki Kuno <knokmki612@gmail.com>
2021-04-27 15:38:54 +0900

    feat: ブックの学習完了はすべてのトピックの学習完了とする

--- utils/getActivitiesByBooks.ts
+++ utils/getActivitiesByBooks.ts
@@ -16,17 +16,22 @@ function getActivitiesByBooks({
     }
   > = [];
   for (const book of courseBooks) {
+    const topicIds = new Set(
+      book.sections.flatMap(({ topics }) => topics).map(({ id }) => id)
+    );
     const activities = bookActivities.filter((a) => a.book.id === book.id);
     const learnerIds = new Set(activities.map((a) => a.learner.id));
     let completedCount = 0;
     for (const learnerId of learnerIds) {
-      const topics = book.sections.flatMap(({ topics }) => topics);
-      const completedActivities = activities.filter(
-        (a) => a.learner.id === learnerId && a.status === "completed"
+      const activitiesByLearner = activities.filter(
+        (a) => a.learner.id === learnerId
       );
-      const completedRate = completedActivities.length / topics.length;
-      // TODO: 完了とみなす割合が決め打ちなので外部化しましょう
-      if (completedRate > 0.8) completedCount++;
+      if (
+        activitiesByLearner.length === topicIds.size &&
+        !activitiesByLearner.some(({ status }) => status !== "completed")
+      ) {
+        completedCount++;
+      }
     }
     const incompletedCount = learnerIds.size - completedCount;
     activitiesByBooks.push({ ...book, completedCount, incompletedCount });
