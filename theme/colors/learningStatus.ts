462a7583b68f6e6f6f528c639c04f5db7d33a8b5
Kimiaki Kuno <knokmki612@gmail.com>
2021-04-27 17:22:10 +0900

    feat: getContrastText()を使う&学習状況に用いる色の見直し

--- theme/colors/learningStatus.ts
+++ theme/colors/learningStatus.ts
@@ -1,9 +1,9 @@
 import gray from "./gray";
 
 const learningStatus = {
-  completed: "#27ae60",
-  incompleted: "#f2c94c",
-  unopened: gray[600],
+  completed: "#2e620c",
+  incompleted: "#9fde77",
+  unopened: gray[300],
 };
 
 export default learningStatus;
