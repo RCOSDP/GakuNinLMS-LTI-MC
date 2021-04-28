65b38011fac5f63e6c0dd2a2bf2afe4668adfc52
Kimiaki Kuno <knokmki612@gmail.com>
2021-04-28 12:11:01 +0900

    feat: @material-ui/colorsから色を使う&明度での識別を意図する旨を明確に

--- theme/colors/learningStatus.ts
+++ theme/colors/learningStatus.ts
@@ -1,9 +1,10 @@
-import gray from "./gray";
+import { green, grey } from "@material-ui/core/colors";
 
+// NOTE: a11yの観点で明度のみによる識別ができるよう上から明度が昇順になっている
 const learningStatus = {
-  completed: "#2e620c",
-  incompleted: "#9fde77",
-  unopened: gray[300],
+  completed: green[900],
+  incompleted: green[300],
+  unopened: grey[300],
 };
 
 export default learningStatus;
