75e5aff23f30e2dfe9218d017c5200bf0809a073
Kimiaki Kuno <knokmki612@gmail.com>
2021-05-17 16:58:06 +0900

    feat: *At以外もカラム名を変更&*AtはISO 8601で出力

--- utils/bookLearningActivity/download.ts
+++ utils/bookLearningActivity/download.ts
@@ -1,8 +1,31 @@
 import { flatten } from "flat";
 import jsonexport from "jsonexport";
+import type { UserHandlers } from "jsonexport";
 import { BookActivitySchema } from "$server/models/bookActivity";
 
 const bom = "\uFEFF";
+
+const jsonexportHandlers: UserHandlers = {
+  mapHeaders: (header) =>
+    header
+      .replace(/^learner\.id$/, "ユーザID")
+      .replace(/^learner\.name$/, "ユーザ名")
+      .replace(/^topic\.id$/, "トピックID")
+      .replace(/^topic\.name$/, "トピック名")
+      .replace(/^completed$/, "完了")
+      .replace(/^totalTimeMs$/, "ユニーク学習時間 (ms)")
+      .replace(/^createdAt$/, "初回アクセス日時")
+      .replace(/^updatedAt$/, "最終アクセス日時")
+      .replace(/^book\.id$/, "ブックID")
+      .replace(/^book\.name$/, "ブック名")
+      .replace(/^status$/, "ステータス"),
+  typeHandlers: {
+    Object: (value) => {
+      if (value instanceof Date) return value.toISOString();
+      return value;
+    },
+  },
+};
 
 /**
  * ブラウザーで学習分析データをCSVファイル(BOM付きUTF-8)に変換しエクスポート
@@ -11,12 +34,7 @@ const bom = "\uFEFF";
  */
 async function download(data: BookActivitySchema[], filename: string) {
   const flattenData = data.map((a) => flatten(a));
-  const csv = await jsonexport(flattenData, {
-    mapHeaders: (header) =>
-      header
-        .replace(/^createdAt$/, "初回アクセス日")
-        .replace(/^updatedAt$/, "最終アクセス日"),
-  });
+  const csv = await jsonexport(flattenData, { ...jsonexportHandlers });
   const file = new File([bom, csv], filename, { type: "text/csv" });
   const dataUrl = URL.createObjectURL(file);
   const anchor = document.createElement("a");
