f7d41607feaf60660cc68ee3324bc38c5a377b52
hATrayflood <h.rayflood@gmail.com>
2021-05-26 14:44:23 +0900

    feat: ブックのインポート実装 part17 class-validatorによるjsonのチェック

--- server/utils/book/importBooksUtil.ts
+++ server/utils/book/importBooksUtil.ts
@@ -1,12 +1,16 @@
+import { validate, ValidationError } from "class-validator";
 import { UserSchema } from "$server/models/user";
 import { BookSchema } from "$server/models/book";
 import {
   BooksImportParams,
   BooksImportResult,
+  ImportTopic,
+  ImportSection,
+  ImportBook,
+  ImportBooks,
 } from "$server/validators/booksImportParams";
 import prisma from "$server/utils/prisma";
 import findBook from "./findBook";
-import parse from "spdx-expression-parse";
 
 async function importBooksUtil(
   authorId: UserSchema["id"],
@@ -18,77 +22,38 @@ async function importBooksUtil(
 }
 
 class ImportBooksUtil {
-  static readonly BOOK_KEYS: string[] = [
-    "name",
-    "description",
-    "language",
-    "shared",
-    "publishedAt",
-    "createdAt",
-    "updatedAt",
-    "details",
-    "keywords",
-    "sections",
-  ];
-  static readonly SECTION_KEYS: string[] = ["name", "topics"];
-  static readonly TOPIC_KEYS: string[] = [
-    "name",
-    "description",
-    "language",
-    "timeRequired",
-    "shared",
-    "createdAt",
-    "updatedAt",
-    "license",
-    "details",
-    "keywords",
-    "resource",
-  ];
-  static readonly RESOURCE_KEYS: string[] = [
-    "url",
-    "providerUrl",
-    "tracks",
-    "details",
-  ];
-  static readonly TRACK_KEYS: string[] = ["kind", "language", "content"];
-
   authorId: UserSchema["id"];
   params: BooksImportParams;
   books: BookSchema[];
   errors: string[];
-
   timeRequired: number;
-  now: Date;
-  errorLabelBook: string;
-  errorLabelSection: string;
-  errorLabelTopic: string;
 
   constructor(authorId: UserSchema["id"], params: BooksImportParams) {
     this.authorId = authorId;
     this.params = params;
     this.books = [];
     this.errors = [];
-
     this.timeRequired = 0;
-    this.now = new Date();
-    this.errorLabelBook = "";
-    this.errorLabelSection = "";
-    this.errorLabelTopic = "";
   }
 
   async importBooks() {
     try {
-      const importBooks = this.parseJson();
+      const importBooks = ImportBooks.init(this.parseJson());
+      if (this.errors.length) return;
+
+      const results = await validate(importBooks, {
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      });
+      this.parseError(results);
       if (this.errors.length) return;
 
       const transactions = [];
-      for (const [index, importBook] of importBooks.entries()) {
-        this.errorLabelBook = `ブック${index + 1}件目`;
+      for (const importBook of importBooks.books) {
         transactions.push(
           prisma.book.create({ data: this.getBookProps(importBook) })
         );
       }
-      //throw "debug";
       if (this.errors.length) return;
 
       for (const book of await prisma.$transaction(transactions)) {
@@ -97,10 +62,33 @@ class ImportBooksUtil {
       }
     } catch (e) {
       console.error(e);
-      Array.prototype.push.apply(
-        this.errors,
-        Array.isArray(e) ? e : [e.toString()]
-      );
+      this.errors.push(...(Array.isArray(e) ? e : [e.toString()]));
+    }
+  }
+
+  parseError(results: ValidationError[], parentLabel = "") {
+    for (const result of results) {
+      const label =
+        parentLabel +
+        (Number.isNaN(Number(result.property))
+          ? parentLabel
+            ? `.${result.property}`
+            : result.property
+          : `[${result.property}]`);
+      for (const constraint in result.constraints) {
+        if (constraint == "whitelistValidation") {
+          this.errors.push(
+            `${parentLabel}: 不明なプロパティ "${result.property}" が含まれています。`
+          );
+        } else if (constraint == "nestedValidation") {
+          this.errors.push(`${parentLabel}: ${result.constraints[constraint]}`);
+        } else {
+          this.errors.push(`${label}: ${result.constraints[constraint]}`);
+        }
+      }
+      if (result.children) {
+        this.parseError(result.children, label);
+      }
     }
   }
 
@@ -114,487 +102,84 @@ class ImportBooksUtil {
 
   parseJson() {
     try {
-      const obj = JSON.parse(this.params.json || "");
-      return Array.isArray(obj) ? obj : [obj];
+      return JSON.parse(this.params.json || "");
     } catch (e) {
       this.errors.push(`入力されたjsonテキストを解釈できません。\n${e}`);
-      return [];
+      return {};
     }
   }
 
-  getBookProps(importBook: any) {
-    const errorLabel = this.errorLabelBook;
-    if (
-      !this.validateObject(
-        importBook,
-        false,
-        `${errorLabel}: ブック情報がありません。`
-      )
-    )
-      return;
-
-    this.validateObjectProperty(
-      importBook,
-      ImportBooksUtil.BOOK_KEYS,
-      errorLabel
-    );
-    const book = {
-      ...importBook,
-      author: { connect: { id: this.authorId } },
-      ltiResourceLinks: {},
-    };
-
-    book.name = this.validateString(book.name, false, "", `${errorLabel} 題名`);
-    book.description = this.validateString(
-      book.description,
-      true,
-      "",
-      `${errorLabel} 概要`
-    );
-    book.language = this.validateString(
-      book.language,
-      true,
-      "ja",
-      `${errorLabel} 言語`
-    );
-    book.shared = this.validateBoolean(
-      book.shared,
-      true,
-      true,
-      `${errorLabel} 共有可否`
-    );
-    book.publishedAt = this.getDate(book.publishedAt, `${errorLabel} 公開日`);
-    book.createdAt = this.getDate(book.createdAt, `${errorLabel} 作成日`);
-    book.updatedAt = this.getDate(book.updatedAt, `${errorLabel} 更新日`);
-    book.keywords = this.getKeywords(book.keywords, `${errorLabel} キーワード`);
-    book.details = this.validateJson(
-      book.details,
-      true,
-      {},
-      `${errorLabel} 詳細`
-    );
-
-    if (!this.validateList(book.sections, false, `${errorLabel} セクション`))
-      return;
+  getBookProps(importBook: ImportBook) {
     const sections = [];
-    for (const [index, bookSection] of book.sections.entries()) {
-      this.errorLabelSection = `セクション${index + 1}件目`;
+    for (const [index, bookSection] of importBook.sections.entries()) {
       sections.push(this.getSection(bookSection, index));
     }
-    book.sections = { create: sections };
-    book.timeRequired = this.timeRequired;
-    return book;
+
+    return {
+      ...importBook,
+      timeRequired: this.timeRequired,
+      author: { connect: { id: this.authorId } },
+      publishedAt: new Date(importBook.publishedAt),
+      createdAt: new Date(importBook.createdAt),
+      updatedAt: new Date(importBook.updatedAt),
+      keywords: this.getKeywords(importBook.keywords),
+      sections: { create: sections },
+      ltiResourceLinks: {},
+    };
   }
 
-  getSection(bookSection: any, order: number) {
-    const errorLabel = `${this.errorLabelBook} ${this.errorLabelSection}`;
-    if (
-      !this.validateObject(
-        bookSection,
-        false,
-        `${errorLabel}: セクション情報がありません。`
-      )
-    )
-      return;
-
-    this.validateObjectProperty(
-      bookSection,
-      ImportBooksUtil.SECTION_KEYS,
-      errorLabel
-    );
-    const section = { order, name: bookSection.name, topicSections: {} };
-    section.name = this.validateString(
-      section.name,
-      true,
-      "",
-      `${errorLabel} 名称`
-    );
-
-    if (!this.validateList(bookSection.topics, false, `${errorLabel} トピック`))
-      return;
+  getSection(bookSection: ImportSection, order: number) {
     const topicSections = [];
     for (const [index, sectionTopic] of bookSection.topics.entries()) {
-      this.errorLabelTopic = `トピック${index + 1}件目`;
       topicSections.push(this.getTopicSection(sectionTopic, index));
     }
-    section.topicSections = { create: topicSections };
-    return section;
+
+    return {
+      order,
+      name: bookSection.name,
+      topicSections: { create: topicSections },
+    };
   }
 
-  getTopicSection(sectionTopic: any, order: number) {
-    const errorLabel = `${this.errorLabelBook} ${this.errorLabelSection} ${this.errorLabelTopic}`;
-    if (
-      !this.validateObject(
-        sectionTopic,
-        false,
-        `${errorLabel}: トピック情報がありません。`
-      )
-    )
-      return;
+  getTopicSection(sectionTopic: ImportTopic, order: number) {
+    this.timeRequired += sectionTopic.timeRequired;
 
-    this.validateObjectProperty(
-      sectionTopic,
-      ImportBooksUtil.TOPIC_KEYS,
-      errorLabel
-    );
+    const video = {
+      create: {
+        providerUrl: sectionTopic.resource.providerUrl,
+        tracks: { create: sectionTopic.resource.tracks },
+      },
+    };
+
+    const resource = {
+      connectOrCreate: {
+        create: {
+          video,
+          url: sectionTopic.resource.url,
+          details: sectionTopic.resource.details,
+        },
+        where: { url: sectionTopic.resource.url },
+      },
+    };
+
     const topic = {
       ...sectionTopic,
       creator: { connect: { id: this.authorId } },
+      createdAt: new Date(sectionTopic.createdAt),
+      updatedAt: new Date(sectionTopic.updatedAt),
+      keywords: this.getKeywords(sectionTopic.keywords),
+      resource,
     };
-    topic.name = this.validateString(
-      topic.name,
-      false,
-      "",
-      `${errorLabel} 名称`
-    );
-    topic.description = this.validateString(
-      topic.description,
-      true,
-      "",
-      `${errorLabel} 説明`
-    );
-    topic.language = this.validateString(
-      topic.language,
-      true,
-      "ja",
-      `${errorLabel} 言語`
-    );
-    topic.timeRequired = this.validateInteger(
-      topic.timeRequired,
-      true,
-      0,
-      `${errorLabel} 学習所要時間`
-    );
-    this.timeRequired += topic.timeRequired;
-    topic.shared = this.validateBoolean(
-      topic.shared,
-      true,
-      true,
-      `${errorLabel} 共有可否`
-    );
-    topic.license = this.validateString(
-      topic.license,
-      true,
-      "",
-      `${errorLabel} ライセンス`
-    );
-    this.validateLicense(topic.license, errorLabel);
-    topic.createdAt = this.getDate(topic.createdAt, `${errorLabel} 作成日`);
-    topic.updatedAt = this.getDate(topic.updatedAt, `${errorLabel} 更新日`);
-    topic.keywords = this.getKeywords(
-      topic.keywords,
-      `${errorLabel} キーワード`
-    );
-    if (
-      !this.validateObject(
-        topic.resource,
-        false,
-        `${errorLabel}: リソース情報がありません。`
-      )
-    )
-      return;
 
-    this.validateObjectProperty(
-      topic.resource,
-      ImportBooksUtil.RESOURCE_KEYS,
-      errorLabel
-    );
-    topic.resource.url = this.validateUrl(
-      topic.resource.url,
-      false,
-      "",
-      `${errorLabel} 外部リソースのアドレス`
-    );
-    topic.resource.video = {
-      create: {
-        providerUrl: this.getProviderUrl(
-          topic.resource.providerUrl,
-          topic.resource.url,
-          `${errorLabel} 動画プロバイダーの識別子`
-        ),
-        tracks: this.getTracks(topic.resource.tracks, `${errorLabel} トラック`),
-      },
-    };
-    topic.resource.details = this.validateJson(
-      topic.resource.details,
-      true,
-      {},
-      `${errorLabel} 外部リソースの詳細`
-    );
-    delete topic.resource.providerUrl;
-    delete topic.resource.tracks;
-
-    topic.resource = {
-      connectOrCreate: {
-        create: topic.resource,
-        where: { url: topic.resource.url },
-      },
-    };
-    topic.details = this.validateJson(
-      topic.details,
-      true,
-      {},
-      `${errorLabel} 詳細`
-    );
-    const topicSection = { order, topic: { create: topic } };
-    return topicSection;
+    return { order, topic: { create: topic } };
   }
 
-  getTracks(tracks: any[], label: string) {
-    if (!this.validateList(tracks, true, label) || !tracks?.length) return {};
-
-    let error = false;
-    for (const [index, track] of tracks.entries()) {
-      const errorLabel = `${label}${index + 1}件目`;
-      const err = !this.validateObject(
-        track,
-        false,
-        `${errorLabel}: トラック情報がありません。`
-      );
-      error ||= err;
-      if (err) continue;
-
-      this.validateObjectProperty(
-        track,
-        ImportBooksUtil.TRACK_KEYS,
-        errorLabel
-      );
-      track.kind = this.validateString(
-        track.kind,
-        true,
-        "",
-        `${errorLabel} 種別`
-      );
-      track.language = this.validateString(
-        track.language,
-        true,
-        "ja",
-        `${errorLabel} 言語`
-      );
-      track.content = this.validateString(
-        track.content,
-        true,
-        "",
-        `${errorLabel} 内容`
-      );
-    }
-
-    return error ? {} : { create: tracks };
-  }
-
-  validateLicense(license: string, label: string) {
-    if (license == null || !license.length) {
-      return;
-    }
-    try {
-      parse(license);
-    } catch (e) {
-      this.errors.push(
-        `${label}: ライセンスを識別できません。https://spdx.org/licenses/ から有効なライセンスを選択してください。\n${e}`
-      );
-    }
-  }
-
-  getProviderUrl(providerUrl: string, url: string, label: string) {
-    if (providerUrl == null) {
-      try {
-        return new URL("/", new URL(url)).toString();
-      } catch (e) {
-        this.errors.push(`${label}: 未指定です。URLを指定してください。`);
-        return "";
-      }
-    } else {
-      return this.validateUrl(providerUrl, false, "", label);
-    }
-  }
-
-  getDate(dateString: string, label: string) {
-    try {
-      const date = dateString ? new Date(dateString) : this.now;
-      if (Number.isNaN(date.getTime())) {
-        this.errors.push(`${label}: 日付を解釈できません。`);
-        return null;
-      }
-      return date;
-    } catch (e) {
-      this.errors.push(`${label}: 日付を解釈できません。`);
-      return null;
-    }
-  }
-
-  getKeywords(keywords: string[], label: string) {
-    if (!this.validateList(keywords, true, label) || !keywords?.length)
-      return {};
-    for (const keyword of keywords) {
-      if (typeof keyword != "string" || !keyword.length) {
-        this.errors.push(`${label}: 文字ではないか空です。`);
-        return {};
-      }
-    }
-
+  getKeywords(keywords: string[]) {
     return {
       connectOrCreate: keywords.map((name) => {
         return { create: { name }, where: { name } };
       }),
     };
-  }
-
-  validateString(
-    value: any,
-    nullable: boolean,
-    defaultValue: string,
-    label: string
-  ) {
-    if (value == null) {
-      if (!nullable)
-        this.errors.push(
-          `${label}: 空文字列です。1文字以上の文字列が必要です。`
-        );
-      return defaultValue;
-    }
-    if (typeof value != "string") {
-      this.errors.push(`${label}: 文字列ではありません。`);
-      return defaultValue;
-    }
-    if (!value.length) {
-      if (!nullable)
-        this.errors.push(
-          `${label}: 空文字列です。1文字以上の文字列が必要です。`
-        );
-      return defaultValue;
-    }
-    return value;
-  }
-
-  validateInteger(
-    value: any,
-    nullable: boolean,
-    defaultValue: number,
-    label: string
-  ) {
-    if (value == null) {
-      if (!nullable)
-        this.errors.push(`${label}: 未指定です。整数値を指定してください。`);
-      return defaultValue;
-    }
-    if (!Number.isInteger(value)) {
-      this.errors.push(`${label}: 整数値ではありません。`);
-      return defaultValue;
-    }
-    return value;
-  }
-
-  validateBoolean(
-    value: any,
-    nullable: boolean,
-    defaultValue: boolean,
-    label: string
-  ) {
-    if (value == null) {
-      if (!nullable)
-        this.errors.push(`${label}: 未指定です。真偽値を指定してください。`);
-      return defaultValue;
-    }
-    if (typeof value != "boolean") {
-      this.errors.push(`${label}: 真偽値ではありません。`);
-      return defaultValue;
-    }
-    return value;
-  }
-
-  validateUrl(
-    value: any,
-    nullable: boolean,
-    defaultValue: string,
-    label: string
-  ) {
-    if (value == null) {
-      if (!nullable)
-        this.errors.push(`${label}: 未指定です。URLを指定してください。`);
-      return defaultValue;
-    }
-    try {
-      new URL(value);
-    } catch (e) {
-      this.errors.push(`${label}: URLではありません。`);
-      return defaultValue;
-    }
-    return value;
-  }
-
-  validateJson(
-    value: any,
-    nullable: boolean,
-    defaultValue: any,
-    label: string
-  ) {
-    if (value == null) {
-      if (!nullable)
-        this.errors.push(
-          `${label}: 未指定です。JSONオブジェクトを指定してください。`
-        );
-      return defaultValue;
-    }
-    if (typeof value != "object") {
-      this.errors.push(`${label}: JSONオブジェクトではありません。`);
-      return defaultValue;
-    }
-    try {
-      JSON.stringify(value);
-    } catch (e) {
-      this.errors.push(`${label}: JSONオブジェクトではありません。`);
-      return defaultValue;
-    }
-    return value;
-  }
-
-  validateObject(value: any, nullable: boolean, label: string) {
-    if (value == null) {
-      if (nullable) {
-        return true;
-      } else {
-        this.errors.push(label);
-        return false;
-      }
-    }
-    if (typeof value != "object" || Array.isArray(value)) {
-      this.errors.push(label);
-      return false;
-    }
-    return true;
-  }
-
-  validateObjectProperty(value: any, allowed: string[], label: string) {
-    const prop = Object.keys(value).filter((key) => !allowed.includes(key));
-    if (prop.length) {
-      this.errors.push(`${label}: 不明なプロパティがあります。${prop}`);
-      return false;
-    }
-    return true;
-  }
-
-  validateList(list: any, nullable: boolean, label: string) {
-    if (list == null) {
-      if (nullable) {
-        return true;
-      } else {
-        this.errors.push(`${label}: 空です。1個以上の要素が必要です。`);
-        return false;
-      }
-    }
-    if (!Array.isArray(list)) {
-      this.errors.push(`${label}: リストになっていません。`);
-      return false;
-    }
-    if (!list.length) {
-      if (nullable) {
-        return true;
-      } else {
-        this.errors.push(`${label}: 空です。1個以上の要素が必要です。`);
-        return false;
-      }
-    }
-    return true;
   }
 }
 
--- server/validators/booksImportParams.ts
+++ server/validators/booksImportParams.ts
@@ -1,5 +1,21 @@
-import { IsOptional, IsString, ValidateNested } from "class-validator";
+import {
+  IsOptional,
+  IsString,
+  IsBoolean,
+  IsInt,
+  Min,
+  MinLength,
+  IsObject,
+  ArrayNotEmpty,
+  IsLocale,
+  IsISO8601,
+  IsUrl,
+  ValidateNested,
+  registerDecorator,
+  ValidatorConstraintInterface,
+} from "class-validator";
 import { validationMetadatasToSchemas } from "class-validator-jsonschema";
+import parse from "spdx-expression-parse";
 import { BookSchema } from "$server/models/book";
 
 export class BooksImportParams {
@@ -22,3 +38,246 @@ export class BooksImportResult {
 
 export const booksImportResultSchema = validationMetadatasToSchemas()
   .BooksImportResult;
+
+class IsLicenseConstraint implements ValidatorConstraintInterface {
+  message = "";
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  validate(value: any) {
+    if (value == null || !value.length) return true;
+    try {
+      parse(value);
+      return true;
+    } catch (e) {
+      this.message = e.toString();
+      return false;
+    }
+  }
+
+  defaultMessage() {
+    return `ライセンスを識別できません。https://spdx.org/licenses/ から有効なライセンスを選択してください。\n${this.message}`;
+  }
+}
+
+function IsLicense() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: "isLicense",
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      validator: new IsLicenseConstraint(),
+    });
+  };
+}
+
+export class ImportTrack {
+  @IsString({ message: "文字列ではないか未設定です。" })
+  @MinLength(1, { message: "空白です。種別を設定してください。" })
+  kind = "subtitles";
+
+  @IsLocale({ message: "正しい言語コードを設定してください。" })
+  language = "ja";
+
+  @IsString({ message: "文字列ではないか未設定です。" })
+  content = "";
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static init(obj: any) {
+    if (typeof obj != "object" || Array.isArray(obj)) return obj;
+
+    return Object.assign(new ImportTrack(), obj);
+  }
+}
+
+export const importTrackSchema = validationMetadatasToSchemas().ImportTrack;
+
+export class ImportResource {
+  @IsUrl({}, { message: "動画ページのURLを設定してください。" })
+  url = "";
+
+  @IsUrl({}, { message: "アップロード先のサイトのURLを設定してください。" })
+  providerUrl = "";
+
+  @IsObject({ message: "オブジェクト型のデータを設定してください。" })
+  details = {};
+
+  @ValidateNested({
+    each: true,
+    message: "トラックではないものが含まれています。",
+  })
+  tracks: ImportTrack[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static init(obj: any) {
+    if (typeof obj != "object" || Array.isArray(obj)) return obj;
+
+    const resource = Object.assign(new ImportResource(), obj);
+    if (!resource.providerUrl) {
+      try {
+        resource.providerUrl = new URL("/", new URL(resource.url)).toString();
+      } catch (e) {
+        // nop
+      }
+    }
+
+    if (Array.isArray(resource.tracks)) {
+      for (const [index, track] of resource.tracks.entries()) {
+        resource.tracks[index] = ImportTrack.init(track);
+      }
+    }
+    return resource;
+  }
+}
+
+export const importResourceSchema = validationMetadatasToSchemas()
+  .ImportResource;
+
+export class ImportTopic {
+  @IsString({ message: "文字列ではないか未設定です。" })
+  @MinLength(1, { message: "空白です。名称を設定してください。" })
+  name = "";
+
+  @IsString({ message: "文字列ではないか未設定です。" })
+  description = "";
+
+  @IsLocale({ message: "正しい言語コードを設定してください。" })
+  language = "ja";
+
+  @IsInt({ message: "整数ではないか未設定です。" })
+  @Min(0, { message: "0以上の整数を設定してください。" })
+  timeRequired = 0;
+
+  @IsBoolean({ message: "真偽値ではないか未設定です。" })
+  shared = true;
+
+  @IsISO8601({}, { message: "ISO8601形式の日時ではないか未設定です。" })
+  createdAt = new Date().toISOString();
+
+  @IsISO8601({}, { message: "ISO8601形式の日時ではないか未設定です。" })
+  updatedAt = new Date().toISOString();
+
+  @IsString({ message: "文字列ではないか未設定です。" })
+  @IsLicense()
+  license = "";
+
+  @IsString({ each: true, message: "文字列のリストを設定してください。" })
+  keywords = [];
+
+  @IsObject({ message: "オブジェクト型のデータを設定してください。" })
+  details = {};
+
+  @ValidateNested({ message: "リソースではないものが含まれています。" })
+  resource: ImportResource = new ImportResource();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static init(obj: any) {
+    if (typeof obj != "object" || Array.isArray(obj)) return obj;
+
+    const topic = Object.assign(new ImportTopic(), obj);
+    topic.resource = ImportResource.init(topic.resource);
+    return topic;
+  }
+}
+
+export const importTopicSchema = validationMetadatasToSchemas().ImportTopic;
+
+export class ImportSection {
+  @IsString({ message: "文字列ではないか未設定です。" })
+  name = "";
+
+  @ValidateNested({
+    each: true,
+    message: "トピックではないものが含まれています。",
+  })
+  @ArrayNotEmpty({ message: "1件以上のトピックが必要です。" })
+  topics: ImportTopic[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static init(obj: any) {
+    if (typeof obj != "object" || Array.isArray(obj)) return obj;
+
+    const section = Object.assign(new ImportSection(), obj);
+    if (Array.isArray(section.topics)) {
+      for (const [index, topic] of section.topics.entries()) {
+        section.topics[index] = ImportTopic.init(topic);
+      }
+    }
+    return section;
+  }
+}
+
+export const importSectionSchema = validationMetadatasToSchemas().ImportSection;
+
+export class ImportBook {
+  @IsString({ message: "文字列ではないか未設定です。" })
+  @MinLength(1, { message: "空白です。題名を設定してください。" })
+  name = "";
+
+  @IsString({ message: "文字列ではないか未設定です。" })
+  description = "";
+
+  @IsLocale({ message: "正しい言語コードを設定してください。" })
+  language = "ja";
+
+  @IsBoolean({ message: "真偽値ではないか未設定です。" })
+  shared = true;
+
+  @IsISO8601({}, { message: "ISO8601形式の日時ではないか未設定です。" })
+  publishedAt = new Date().toISOString();
+
+  @IsISO8601({}, { message: "ISO8601形式の日時ではないか未設定です。" })
+  createdAt = new Date().toISOString();
+
+  @IsISO8601({}, { message: "ISO8601形式の日時ではないか未設定です。" })
+  updatedAt = new Date().toISOString();
+
+  @IsString({ each: true, message: "文字列のリストを設定してください。" })
+  keywords = [];
+
+  @IsObject({ message: "オブジェクト型のデータを設定してください。" })
+  details = {};
+
+  @ValidateNested({
+    each: true,
+    message: "セクションではないものが含まれています。",
+  })
+  @ArrayNotEmpty({ message: "1件以上のセクションが必要です。" })
+  sections: ImportSection[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static init(obj: any) {
+    if (typeof obj != "object" || Array.isArray(obj)) return obj;
+
+    const book = Object.assign(new ImportBook(), obj);
+    if (Array.isArray(book.sections)) {
+      for (const [index, section] of book.sections.entries()) {
+        book.sections[index] = ImportSection.init(section);
+      }
+    }
+    return book;
+  }
+}
+
+export const importBookSchema = validationMetadatasToSchemas().ImportBook;
+
+export class ImportBooks {
+  @ValidateNested({
+    each: true,
+    message: "ブックではないものが含まれています。",
+  })
+  @ArrayNotEmpty({ message: "1件以上のブックが必要です。" })
+  books: ImportBook[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static init(obj: any) {
+    const books = new ImportBooks();
+    for (const book of Array.isArray(obj) ? obj : [obj]) {
+      books.books.push(ImportBook.init(book));
+    }
+    return books;
+  }
+}
+
+export const importBooksSchema = validationMetadatasToSchemas().ImportBooks;
