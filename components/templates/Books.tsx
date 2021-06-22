6081e7a550053d5debba80e71f09e6be91df75cf
Kimiaki Kuno <knokmki612@gmail.com>
2021-06-22 17:03:09 +0900

    docs: NOTEは「なぜそうしたか」背景を明らかにすべき

--- components/templates/Books.tsx
+++ components/templates/Books.tsx
@@ -27,11 +27,11 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(2),
   },
   linkedBook: {
-    // NOTE: 決め打ち (動画プレイヤーの高さに依存)
+    // NOTE: BookPreviewに依存する&事前に高さを確定できないので決め打ち
     minHeight: 255,
   },
   linkedBookPlaceholder: {
-    // NOTE: 決め打ち (動画プレイヤーの高さに依存)
+    // NOTE: BookPreviewに依存する&事前に高さを確定できないので決め打ち
     minHeight: 205,
     display: "flex",
     alignItems: "center",
