6f882508a41278d12bff4a39e1bbc9ac5dce436d
Kimiaki Kuno <knokmki612@gmail.com>
2021-07-09 10:47:39 +0900

    feat: ツリー表示風のセクションリスト

--- components/organisms/BookChildren.tsx
+++ components/organisms/BookChildren.tsx
@@ -1,14 +1,10 @@
-import { ReactNode, MouseEvent, useState } from "react";
+import { ReactNode, MouseEvent } from "react";
 import clsx from "clsx";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import Collapse from "@material-ui/core/Collapse";
 import { makeStyles } from "@material-ui/styles";
-import ExpandLessIcon from "@material-ui/icons/ExpandLess";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import EditButton from "$atoms/EditButton";
+import BookChildrenItem from "$atoms/BookChildrenItem";
 import { TopicSchema } from "$server/models/topic";
 import { SectionSchema } from "$server/models/book/section";
 import { primary } from "$theme/colors";
@@ -17,28 +13,29 @@ import { isNamedSection, getOutlineNumber } from "$utils/outline";
 function Section({
   section,
   sectionItemIndex,
+  end,
   onSectionClick,
-  open,
   children,
 }: {
   section: Pick<SectionSchema, "name" | "topics">;
   sectionItemIndex: number;
-  onSectionClick(): void;
-  open: boolean;
+  end: boolean;
+  onSectionClick(event: MouseEvent<HTMLElement>): void;
   children: ReactNode;
 }) {
   if (!isNamedSection(section)) return <>{children}</>;
 
   return (
     <>
-      <ListItem button onClick={onSectionClick}>
-        <ListItemText>
-          {getOutlineNumber(section, sectionItemIndex) + " "}
-          {section.name ?? "無名のセクション"}
-        </ListItemText>
-        {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-      </ListItem>
-      <Collapse in={open}>{children}</Collapse>
+      <BookChildrenItem
+        variant="section"
+        outlineNumber={getOutlineNumber(section, sectionItemIndex)}
+        name={section.name}
+        end={end}
+        button
+        onClick={onSectionClick}
+      />
+      {children}
     </>
   );
 }
@@ -68,18 +65,13 @@ export default function BookChildren(props: Props) {
     onItemEditClick,
   } = props;
   const classes = useStyles();
-  const [open, setOpen] = useState<boolean[]>(sections.map(() => true));
   const handleItemClick = (event: MouseEvent<HTMLElement>) => {
     const { section, topic } = event.currentTarget.dataset;
     onItemClick(event, ([section, topic].map(Number) as unknown) as ItemIndex);
   };
-  const handleSectionClick = (sectionItemIndex: number) => () => {
-    setOpen((open) => {
-      const newOpen = open.slice();
-      newOpen[sectionItemIndex] = !newOpen[sectionItemIndex];
-      return newOpen;
-    });
-  };
+  const handleSectionClick = (sectionItemIndex: number) => (
+    event: MouseEvent<HTMLElement>
+  ) => onItemClick(event, [sectionItemIndex, 0]);
   const handleItemEditClick = (...index: ItemIndex) => (
     event: MouseEvent<HTMLButtonElement>
   ) => {
@@ -93,27 +85,36 @@ export default function BookChildren(props: Props) {
           key={section.id}
           section={section}
           sectionItemIndex={sectionItemIndex}
+          end={sectionItemIndex === sections.length - 1}
           onSectionClick={handleSectionClick(sectionItemIndex)}
-          open={open[sectionItemIndex]}
         >
           {section.topics.map((topic, topicItemIndex) => (
-            <ListItem
+            <BookChildrenItem
               key={`${topic.id}:${topicItemIndex}`}
               className={clsx({
                 [classes.active]:
                   sectionIndex === sectionItemIndex &&
                   topicIndex === topicItemIndex,
               })}
+              variant="topic"
+              outlineNumber={getOutlineNumber(
+                section,
+                sectionItemIndex,
+                topicItemIndex
+              )}
+              name={topic.name}
+              end={
+                (section.topics.length > 1 &&
+                  topicItemIndex === section.topics.length - 1) ||
+                (section.topics.length === 1 &&
+                  sectionItemIndex === sections.length - 1)
+              }
+              depth={section.topics.length > 1 ? 1 : 0}
               button
               data-section={sectionItemIndex}
               data-topic={topicItemIndex}
               onClick={handleItemClick}
             >
-              <ListItemText>
-                {getOutlineNumber(section, sectionItemIndex, topicItemIndex) +
-                  " "}
-                {topic.name}
-              </ListItemText>
               {isTopicEditable(topic) && onItemEditClick && (
                 <ListItemSecondaryAction>
                   <EditButton
@@ -126,7 +127,7 @@ export default function BookChildren(props: Props) {
                   />
                 </ListItemSecondaryAction>
               )}
-            </ListItem>
+            </BookChildrenItem>
           ))}
         </Section>
       ))}
