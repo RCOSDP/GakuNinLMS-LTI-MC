4a3852f22df14ddf5aa041f66715b00b966406ed
Kimiaki Kuno <knokmki612@gmail.com>
2021-08-06 15:17:53 +0900

    fix: 型定義が正確でない

--- types/videojsHlsjsPlugin.d.ts
+++ types/videojsHlsjsPlugin.d.ts
@@ -1,6 +1,6 @@
 declare module "@meikidd/videojs-hlsjs-plugin/lib/videojs-hlsjs-plugin.js" {
   import type videojs from "video.js";
-  type VideoJs = Parameters<typeof videojs>[0];
+  type VideoJs = typeof videojs;
   export function registerConfigPlugin(videojs: VideoJs);
   export function registerSourceHandler(videojs: VideoJs);
 }
