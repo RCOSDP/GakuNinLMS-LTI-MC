0c4c88f6ff6e936cde95756ac0296f1a86f8b95f
hATrayflood <h.rayflood@gmail.com>
2021-09-17 17:46:56 +0900

    feat: zoomインポート処理とバッチ化 part7.2

--- server/prisma/migrations/20210917084244_add_zoom_meetings/migration.sql
+++ server/prisma/migrations/20210917084244_add_zoom_meetings/migration.sql
@@ -6,5 +6,8 @@ CREATE TABLE "zoom_meetings" (
     PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "zoom_meetings.resource_id_unique" ON "zoom_meetings"("resource_id");
+
 -- AddForeignKey
 ALTER TABLE "zoom_meetings" ADD FOREIGN KEY ("resource_id") REFERENCES "resources"("id") ON DELETE CASCADE ON UPDATE CASCADE;
