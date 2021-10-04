d12369cab1cad3f1a762171c2113a5d6c94ab93d
Kimiaki Kuno <knokmki612@gmail.com>
2021-10-04 19:04:00 +0900

    fix: muiが定義している色への参照方法が適切でない

--- styles/select.ts
+++ styles/select.ts
@@ -1,9 +1,8 @@
 import makeStyles from "@mui/styles/makeStyles";
-import common from "@mui/material/colors/common";
 
 const select = makeStyles((theme) => ({
   select: {
-    backgroundColor: common.white,
+    backgroundColor: theme.palette.common.white,
     padding: theme.spacing(1.25, 1.75),
     "&:focus": {
       borderRadius: "6px",
