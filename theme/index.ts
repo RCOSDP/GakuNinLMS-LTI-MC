99fee26d175f8480f5d6dbdb9fe12d9b4ae35078
Kimiaki Kuno <knokmki612@gmail.com>
2021-10-04 18:35:09 +0900

    refactor: MuiThemeはadaptV5Themeを用いず定義するように

--- theme/index.ts
+++ theme/index.ts
@@ -1,18 +1,16 @@
-import { createTheme, adaptV4Theme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 import { primary } from "./colors";
 
-export default createTheme(
-  adaptV4Theme({
-    palette: {
-      primary: {
-        light: primary[300],
-        main: primary[500],
-        dark: primary[700],
-        contrastText: "#fff",
-      },
+export default createTheme({
+  palette: {
+    primary: {
+      light: primary[300],
+      main: primary[500],
+      dark: primary[700],
+      contrastText: "#fff",
     },
-    typography: {
-      fontFamily: "sans-serif",
-    },
-  })
-);
+  },
+  typography: {
+    fontFamily: "sans-serif",
+  },
+});
