4351aa2f914d6b350e6f17abd6f01379bccfd504
Kimiaki Kuno <knokmki612@gmail.com>
2021-10-04 16:03:54 +0900

    fix(AppBarNavButton): flex-directionが適切な方向ではない

--- components/atoms/AppBarNavButton.tsx
+++ components/atoms/AppBarNavButton.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 import makeStyles from "@mui/styles/makeStyles";
 
 const useButtonStyles = makeStyles({
-  label: {
+  root: {
     flexDirection: "column",
     lineHeight: 1.5,
   },
