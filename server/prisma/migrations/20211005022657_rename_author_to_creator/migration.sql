35bf749dacc4503978caf774aa154fc4858a654f
Kimiaki Kuno <knokmki612@gmail.com>
2021-10-05 17:04:04 +0900

    refactor: 冗長な {DROP,ADD} FOUREIGN KEY

--- server/prisma/migrations/20211005022657_rename_author_to_creator/migration.sql
+++ server/prisma/migrations/20211005022657_rename_author_to_creator/migration.sql
@@ -5,11 +5,11 @@
   - You are about to rename the `author_id` column of the `lti_resource_link` table to `creator_id`.
 
 */
--- DropForeignKey
-ALTER TABLE "books" DROP CONSTRAINT "books_author_id_fkey";
+-- RenameForeignKey
+ALTER TABLE "books" RENAME CONSTRAINT "books_author_id_fkey" TO "books_creator_id_fkey";
 
--- DropForeignKey
-ALTER TABLE "lti_resource_link" DROP CONSTRAINT "lti_resource_link_author_id_fkey";
+-- RenameForeignKey
+ALTER TABLE "lti_resource_link" RENAME CONSTRAINT "lti_resource_link_author_id_fkey" TO "lti_resource_link_creator_id_fkey";
 
 -- AlterIndex
 ALTER INDEX "author_id" RENAME TO "creator_id";
@@ -19,9 +19,3 @@ ALTER TABLE "books" RENAME COLUMN "author_id" TO "creator_id";
 
 -- AlterTable
 ALTER TABLE "lti_resource_link" RENAME COLUMN "author_id" TO "creator_id";
-
--- AddForeignKey
-ALTER TABLE "books" ADD FOREIGN KEY ("creator_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "lti_resource_link" ADD FOREIGN KEY ("creator_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
