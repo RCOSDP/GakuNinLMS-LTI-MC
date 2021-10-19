c2feef3d7902698d8ae52d024765327003fc17d1
Kimiaki Kuno <knokmki612@gmail.com>
2021-10-19 17:35:09 +0900

    refactor: 参照するcssクラス名をハードコーディングしない

--- components/atoms/Autocomplete.tsx
+++ components/atoms/Autocomplete.tsx
@@ -1,10 +1,11 @@
-import Autocomplete from "@mui/material/Autocomplete";
+import Autocomplete, { autocompleteClasses } from "@mui/material/Autocomplete";
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
 import { styled } from "@mui/material/styles";
 
 export default styled(Autocomplete)(({ theme }) => ({
-  "& .MuiOutlinedInput-root": {
+  [`.${outlinedInputClasses.root}`]: {
     padding: 0,
-    "& .MuiAutocomplete-input": {
+    [`.${autocompleteClasses.input}`]: {
       padding: theme.spacing(1.25, 1.75),
     },
   },
