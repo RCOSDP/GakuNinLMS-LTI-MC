1a724e1619577a236f0c89ab8926b803cbe0894b
Kimiaki Kuno <knokmki612@gmail.com>
2021-10-22 16:23:35 +0900

    feat: コンテンツ著者APIのハンドラーへの注入

--- utils/useAuthorsHandler.ts
+++ utils/useAuthorsHandler.ts
@@ -1,31 +1,48 @@
 import { useEffect } from "react";
 import type { Content } from "$types/content";
+import type { BookSchema } from "$server/models/book";
 import { AuthorSchema } from "$server/models/author";
 import type { AuthorProps } from "$server/models/author";
 import { useAuthorsAtom } from "$store/authors";
 import { fetchUsers } from "$utils/users";
+import { updateBookAuthors } from "$utils/bookAuthors";
+import { updateTopicAuthors } from "$utils/topicAuthors";
+
+const isBookSchema = (content: Content): content is BookSchema =>
+  "sections" in content;
+
+const updateContentAuthors = (content: Content, authors: AuthorSchema[]) =>
+  isBookSchema(content)
+    ? updateBookAuthors({ id: content.id, authors })
+    : updateTopicAuthors({ id: content.id, authors });
 
 function useAuthorsHandler(content?: Content) {
-  const { authors, updateAuthors } = useAuthorsAtom();
+  const { authors: authorsState, updateAuthors } = useAuthorsAtom();
   useEffect(() => {
     updateAuthors(content?.authors ?? []);
   }, [content, updateAuthors]);
+  async function handleAuthorsUpdate(authors: AuthorSchema[]) {
+    content && updateContentAuthors(content, authors);
+    updateAuthors(authors);
+  }
   async function handleAuthorSubmit({ email }: AuthorProps) {
     if (!email) return;
     const users = await fetchUsers(email);
-    updateAuthors([
-      ...authors,
+    const authors = [
+      ...authorsState,
       ...users
-        .filter((user) => !authors.some((author) => author.id === user.id))
+        .filter((user) => !authorsState.some((author) => author.id === user.id))
         .map((user) => ({
           ...user,
           roleName: AuthorSchema._roleNames.author,
         })),
-    ]);
+    ];
+    content && updateContentAuthors(content, authors);
+    updateAuthors(authors);
   }
 
   return {
-    handleAuthorsUpdate: updateAuthors,
+    handleAuthorsUpdate,
     handleAuthorSubmit,
   };
 }
