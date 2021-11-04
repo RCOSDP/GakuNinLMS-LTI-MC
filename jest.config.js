b91fd3ade8edf3305c3f9a893bd5eeb0346a14fe
Kohei Watanabe <kou029w@gmail.com>
2021-11-04 23:35:19 +0900

    perf: ts-jest isolatedModules オプション有効化によるパフォーマンス改善
    [Isolated Modules option \| ts\-jest](https://kulshekhar.github.io/ts-jest/docs/26.5/getting-started/options/isolatedModules/#performance)

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const { compilerOptions } = require("./tsconfig.json");
 
 module.exports = {
   preset: "ts-jest",
+  globals: { "ts-jest": { isolatedModules: true } },
   testPathIgnorePatterns: workspaces,
   projects: [__dirname, ...workspaces],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
