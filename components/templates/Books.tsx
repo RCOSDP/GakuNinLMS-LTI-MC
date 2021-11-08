d34162124e49ee06e55cebe40b1896b9f42e7521
Kimiaki Kuno <knokmki612@gmail.com>
2021-11-08 17:38:48 +0900

    feat: ブックの提供先が分かるLinkInfoの実装

--- components/templates/Books.tsx
+++ components/templates/Books.tsx
@@ -2,13 +2,11 @@ import useInfiniteScroll from "react-infinite-scroll-hook";
 import Skeleton from "@mui/material/Skeleton";
 import makeStyles from "@mui/styles/makeStyles";
 import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import AddIcon from "@mui/icons-material/Add";
 import ActionHeader from "$organisms/ActionHeader";
-import BookPreview from "$organisms/BookPreview";
 import ContentPreview from "$organisms/ContentPreview";
+import LinkInfo from "$organisms/LinkInfo";
 import SortSelect from "$atoms/SortSelect";
 import AuthorFilter from "$atoms/AuthorFilter";
 import SearchTextField from "$atoms/SearchTextField";
@@ -17,23 +15,11 @@ import type { LinkedBook } from "$types/linkedBook";
 import type { SortOrder } from "$server/models/sortOrder";
 import type { Filter } from "$types/filter";
 import useContainerStyles from "styles/container";
-import useCardStyles from "$styles/card";
 import { useSearchAtom } from "$store/search";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(0.5),
-  },
-  title: {
-    marginBottom: theme.spacing(2),
-  },
-  linkedBookPlaceholder: {
-    // NOTE: BookPreviewに依存する&事前に高さを確定できないので決め打ち
-    height: 180,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderStyle: "dashed",
   },
   books: {
     display: "grid",
@@ -80,7 +66,6 @@ export default function Books(props: Props) {
   const handleBooksImportClick = () => onBooksImportClick();
   const classes = useStyles();
   const containerClasses = useContainerStyles();
-  const cardClasses = useCardStyles();
   const [infiniteRef] = useInfiniteScroll({ loading, hasNextPage, onLoadMore });
   return (
     <div ref={infiniteRef}>
@@ -104,35 +89,7 @@ export default function Books(props: Props) {
           </>
         }
         body={
-          <>
-            <Typography className={classes.title} variant="h5">
-              提供中のブック
-            </Typography>
-            {linkedBook && (
-              <BookPreview
-                book={{ ...linkedBook, ltiResourceLinks: [] }}
-                linked
-                onBookPreviewClick={onBookPreviewClick}
-                onBookEditClick={
-                  linkedBook.editable ? onBookEditClick : undefined
-                }
-                onLinkedBookClick={onLinkedBookClick}
-                onLtiContextClick={onLtiContextClick}
-              />
-            )}
-            {!linkedBook && (
-              <Card
-                classes={cardClasses}
-                className={classes.linkedBookPlaceholder}
-              >
-                <Typography variant="body2">
-                  提供中のブックがありません。
-                  <br />
-                  ブックを提供するには、提供したいブックの「このブックを提供」ボタンをクリックしてください。
-                </Typography>
-              </Card>
-            )}
-          </>
+          <LinkInfo book={linkedBook} onLinkedBookClick={onLinkedBookClick} />
         }
         action={
           <>
