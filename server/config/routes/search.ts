c587c18fff15d690bb342c56cba803e8e5c1c357
Kohei Watanabe <kou029w@gmail.com>
2021-11-09 15:53:29 +0900

    refactor: rename router function to /search

--- server/config/routes/search.ts
+++ server/config/routes/search.ts
@@ -3,7 +3,7 @@ import makeHooks from "$server/utils/makeHooks";
 import handler from "$server/utils/handler";
 import * as service from "$server/services/search";
 
-export async function users(fastify: FastifyInstance) {
+export async function search(fastify: FastifyInstance) {
   const path = "/search";
   const { method, index } = service;
   const hooks = makeHooks(fastify, service.hooks);
