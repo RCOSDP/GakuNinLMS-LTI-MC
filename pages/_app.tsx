dbe48543ea3aeba6fc40a47f86649eb3955cd761
ties-makimura <makimura@cccties.org>
2021-11-15 17:22:06 +0900

    fix: ブック提供はブック閲覧画面でおこなうようになっている
    https://github.com/npocccties/chibichilo/commit/71f847957a136a727801c4d61acd6089af918f6d#diff-a9827ce1be32c607c948881a89b99c592f752898f9c49e0dea0b7199eb151029

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,7 +41,6 @@ function Content({ children }: { children: ReactNode }) {
 
   const handleBooksClick = () => router.push(pagesPath.books.$url());
   const handleTopicsClick = () => router.push(pagesPath.topics.$url());
-  const handleBookLinkClick = () => router.push(pagesPath.$url());
   const handleDashboardClick = () => router.push(pagesPath.dashboard.$url());
   const handleBookClick = () => router.push(pagesPath.$url());
 
@@ -54,7 +53,6 @@ function Content({ children }: { children: ReactNode }) {
             session={session}
             onBooksClick={handleBooksClick}
             onTopicsClick={handleTopicsClick}
-            onBookLinkClick={handleBookLinkClick}
             onDashboardClick={handleDashboardClick}
             onBookClick={handleBookClick}
           />
