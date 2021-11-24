6e3a0b404e43e023b0253f6cded9b02b9984e749
Kimiaki Kuno <knokmki612@gmail.com>
2021-11-24 21:00:47 +0900

    refactor: 冗長なパターンマッチ

--- server/prisma/migrations/20211124170332_set_keyword_zoom/migration.sql
+++ server/prisma/migrations/20211124170332_set_keyword_zoom/migration.sql
@@ -4,4 +4,4 @@ INSERT INTO "keywords" ("name") VALUES ('Zoom') ON CONFLICT DO NOTHING;
 --- Set `A` `B`
 INSERT INTO "_KeywordToTopic"
 SELECT "keywords"."id" AS "A", "topics"."id" AS "B" FROM "keywords" CROSS JOIN "topics"
-WHERE "keywords"."name" = 'Zoom' AND "topics"."name" LIKE '%' || (U&'\+01F4FD') || '%';
+WHERE "keywords"."name" = 'Zoom' AND "topics"."name" LIKE E'\U0001F4FD%';
