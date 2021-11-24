6c0e2eba023bd0df8db20a101e6d76e53a2e2669
Kimiaki Kuno <knokmki612@gmail.com>
2021-11-24 19:40:33 +0900

    feat: Zoomインポートされたトピックをキーワード付きへ移行

---
+++ server/prisma/migrations/20211124170332_set_keyword_zoom/migration.sql
@@ -0,0 +1,7 @@
+--- Set `name`
+INSERT INTO "keywords" ("id", "name") VALUES (DEFAULT, 'Zoom') ON CONFLICT DO NOTHING;
+
+--- Set `A` `B`
+INSERT INTO "_KeywordToTopic"
+SELECT "keywords"."id" AS "A", "topics"."id" AS "B" FROM "keywords" CROSS JOIN "topics"
+WHERE "keywords"."name" = 'Zoom' AND "topics"."name" LIKE '%' || (U&'\+01F4FD') || '%';
