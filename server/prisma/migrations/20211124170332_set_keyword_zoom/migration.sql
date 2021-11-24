6a454b1ca3b7a25eebdb642c18e8b692511f7c00
Kimiaki Kuno <knokmki612@gmail.com>
2021-11-24 21:01:26 +0900

    refactor: 冗長なテーブル式

--- server/prisma/migrations/20211124170332_set_keyword_zoom/migration.sql
+++ server/prisma/migrations/20211124170332_set_keyword_zoom/migration.sql
@@ -3,5 +3,5 @@ INSERT INTO "keywords" ("name") VALUES ('Zoom') ON CONFLICT DO NOTHING;
 
 --- Set `A` `B`
 INSERT INTO "_KeywordToTopic"
-SELECT "keywords"."id" AS "A", "topics"."id" AS "B" FROM "keywords" CROSS JOIN "topics"
+SELECT "keywords"."id" AS "A", "topics"."id" AS "B" FROM "keywords", "topics"
 WHERE "keywords"."name" = 'Zoom' AND "topics"."name" LIKE E'\U0001F4FD%';
