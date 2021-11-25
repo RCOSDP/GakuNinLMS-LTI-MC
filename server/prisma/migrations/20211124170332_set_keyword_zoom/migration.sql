d5951fe9031cf9010feb7a6878fc7aa5dcce4977
Kimiaki Kuno <knokmki612@gmail.com>
2021-11-25 12:09:23 +0900

    fix: 移行時挿入されるレコードが重複しているとエラーになる

--- server/prisma/migrations/20211124170332_set_keyword_zoom/migration.sql
+++ server/prisma/migrations/20211124170332_set_keyword_zoom/migration.sql
@@ -4,4 +4,5 @@ INSERT INTO "keywords" ("name") VALUES ('Zoom') ON CONFLICT DO NOTHING;
 --- Set `A` `B`
 INSERT INTO "_KeywordToTopic"
 SELECT "keywords"."id" AS "A", "topics"."id" AS "B" FROM "keywords", "topics"
-WHERE "keywords"."name" LIKE 'Zoom' AND "topics"."name" LIKE E'\U0001F4FD%';
+WHERE "keywords"."name" LIKE 'Zoom' AND "topics"."name" LIKE E'\U0001F4FD%'
+ON CONFLICT DO NOTHING;
