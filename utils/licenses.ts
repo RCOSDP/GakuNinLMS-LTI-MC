d5085ff0e011001104016f186e80946a712f9e0f
Kimiaki Kuno <knokmki612@gmail.com>
2021-12-06 15:12:48 +0900

    feat: ライセンス選択時の文言の調整
    ref #598

--- utils/licenses.ts
+++ utils/licenses.ts
@@ -4,32 +4,32 @@ const licenses: {
   "CC-BY-4.0": {
     button: "https://i.creativecommons.org/l/by/4.0/88x31.png",
     url: "https://creativecommons.org/licenses/by/4.0/",
-    name: "クリエイティブ・コモンズ 表示 4.0 国際 ライセンス",
+    name: "CC BY（表示）",
   },
   "CC-BY-NC-4.0": {
     button: "https://i.creativecommons.org/l/by-nc/4.0/88x31.png",
     url: "https://creativecommons.org/licenses/by-nc/4.0/",
-    name: "クリエイティブ・コモンズ 表示 - 非営利 4.0 国際 ライセンス",
+    name: "CC BY-NC（表示-非営利）",
   },
   "CC-BY-NC-ND-4.0": {
     button: "https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png",
     url: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
-    name: "クリエイティブ・コモンズ 表示 - 非営利 - 改変禁止 4.0 国際 ライセンス",
+    name: "CC BY-NC-ND（表示-非営利-改変禁止）",
   },
   "CC-BY-NC-SA-4.0": {
     button: "https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png",
     url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
-    name: "クリエイティブ・コモンズ 表示 - 非営利 - 継承 4.0 国際 ライセンス",
+    name: "CC BY-NC-SA（表示-非営利-継承）",
   },
   "CC-BY-ND-4.0": {
     button: "https://i.creativecommons.org/l/by-nd/4.0/88x31.png",
     url: "https://creativecommons.org/licenses/by-nd/4.0/",
-    name: "クリエイティブ・コモンズ 表示 - 改変禁止 4.0 国際 ライセンス",
+    name: "CC BY-ND（表示-改変禁止）",
   },
   "CC-BY-SA-4.0": {
     button: "https://i.creativecommons.org/l/by-sa/4.0/88x31.png",
     url: "https://creativecommons.org/licenses/by-sa/4.0/",
-    name: "クリエイティブ・コモンズ 表示 - 継承 4.0 国際 ライセンス",
+    name: "CC BY-SA（表示-継承）",
   },
 } as const;
 
