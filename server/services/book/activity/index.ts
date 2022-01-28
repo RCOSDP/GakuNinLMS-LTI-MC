190facfd4823d134f0ff44c717929829dfa9c0c9
Kohei Watanabe <kou029w@gmail.com>
2022-01-28 16:13:11 +0900

    chore: format
    ```
    yarn format
    ```

--- server/services/book/activity/index.ts
+++ server/services/book/activity/index.ts
@@ -1,5 +1,5 @@
 import { outdent } from "outdent";
-import type { BookParams} from "$server/validators/bookParams";
+import type { BookParams } from "$server/validators/bookParams";
 import { bookParamsSchema } from "$server/validators/bookParams";
 import type { SessionSchema } from "$server/models/session";
 import { ActivitySchema } from "$server/models/activity";
