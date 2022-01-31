1aae17ab34bf9c91b7029b1f5300df7eb1d3f298
Kimiaki Kuno <knokmki612@gmail.com>
2022-01-31 11:20:33 +0900

    refactor: KeywordChipとして外部化

---
+++ components/atoms/KeywordChip.tsx
@@ -0,0 +1,35 @@
+import { styled } from "@mui/material/styles";
+import MuiChip from "@mui/material/Chip";
+import type { SxProps } from "@mui/system";
+import type { KeywordPropSchema } from "$server/models/keyword";
+
+type Props = {
+  keyword: KeywordPropSchema;
+  onKeywordClick?(keyword: KeywordPropSchema): void;
+  sx?: SxProps;
+  onDelete?: () => void;
+};
+
+const Chip = styled(MuiChip)({
+  borderRadius: 4,
+});
+
+export default function KeywordChip({
+  keyword,
+  onKeywordClick,
+  sx,
+  onDelete,
+}: Props) {
+  const handleClick = () => onKeywordClick?.(keyword);
+  return (
+    <Chip
+      sx={sx}
+      variant="outlined"
+      size="small"
+      color="primary"
+      label={keyword.name}
+      onClick={onKeywordClick && handleClick}
+      onDelete={onDelete}
+    />
+  );
+}
