45937cabeefa70ec2d7dfe6b606f427f457823e6
Kohei Watanabe <kou029w@gmail.com>
2022-02-15 11:00:54 +0900

    feat: コースごとでの学習活動の取得

--- utils/useActivity.ts
+++ utils/useActivity.ts
@@ -3,11 +3,14 @@ import type { LearnerSchema } from "$server/models/learner";
 import type { CourseBookSchema } from "$server/models/courseBook";
 import type { BookActivitySchema } from "$server/models/bookActivity";
 import { api } from "./api";
+import { NEXT_PUBLIC_ACTIVITY_LTI_CONTEXT_ONLY } from "./env";
 
 const key = "/api/v2/activity";
 
 async function fetchActivity() {
-  const res = await api.apiV2ActivityGet({});
+  const res = await api.apiV2ActivityGet({
+    currentLtiContextOnly: NEXT_PUBLIC_ACTIVITY_LTI_CONTEXT_ONLY,
+  });
   const learners = res["learners"] as Array<LearnerSchema>;
   const courseBooks = res["courseBooks"] as Array<CourseBookSchema>;
   const bookActivities = res["bookActivities"] as Array<BookActivitySchema>;
--- utils/useBookActivity.ts
+++ utils/useBookActivity.ts
@@ -3,12 +3,18 @@ import type { BookSchema } from "$server/models/book";
 import { api } from "./api";
 import type { ActivitySchema } from "$server/models/activity";
 import { useActivityAtom } from "$store/activity";
-import { NEXT_PUBLIC_ACTIVITY_SEND_INTERVAL } from "./env";
+import {
+  NEXT_PUBLIC_ACTIVITY_LTI_CONTEXT_ONLY,
+  NEXT_PUBLIC_ACTIVITY_SEND_INTERVAL,
+} from "./env";
 
 const key = "/api/v2/book/{book_id}/activity";
 
 async function fetchBookActivity(_: typeof key, bookId: BookSchema["id"]) {
-  const res = await api.apiV2BookBookIdActivityGet({ bookId });
+  const res = await api.apiV2BookBookIdActivityGet({
+    bookId,
+    currentLtiContextOnly: NEXT_PUBLIC_ACTIVITY_LTI_CONTEXT_ONLY,
+  });
   return res.activity as Array<ActivitySchema>;
 }
 
