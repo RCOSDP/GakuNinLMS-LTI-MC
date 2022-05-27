bc482ca0088608f65d37582f8d3a2c153431ab36
hATrayflood <h.rayflood@gmail.com>
2022-05-27 12:43:50 +0900

    fix: ブックプレビュー時に動画が表示されないことがある問題を修正 part2

--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -11,6 +11,7 @@ import { pagesPath } from "$utils/$path";
 import { updateLtiResourceLink } from "$utils/ltiResourceLink";
 import getLtiResourceLink from "$utils/getLtiResourceLink";
 import useDialogProps from "$utils/useDialogProps";
+import { useBookAtom } from "$store/book";
 import { useSearchAtom } from "$store/search";
 import { revalidateContents } from "utils/useContents";
 
@@ -27,10 +28,16 @@ function Index() {
   const { linkedBook } = useLinkedBook();
   const {
     data: previewContent,
-    dispatch: onContentPreviewClick,
+    dispatch,
     ...dialogProps
   } = useDialogProps<ContentSchema>();
+  const { updateBook } = useBookAtom();
+
   const { query } = useSearchAtom();
+  const onContentPreviewClick = (book: ContentSchema) => {
+    updateBook(book as BookSchema);
+    dispatch(book);
+  };
   const onContentEditClick = (book: Pick<ContentSchema, "id" | "authors">) => {
     const action = isContentEditable(book) ? "edit" : "generate";
     return router.push(
