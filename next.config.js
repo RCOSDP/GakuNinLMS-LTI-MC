bf199ed4d72dbb519d74f097d591210b7693cd35
Kohei Watanabe <kou029w@gmail.com>
2022-08-05 15:56:20 +0900

    fix: 依存関係の更新 (next, storybook, webpack)
    - Next.js v12 https://nextjs.org/blog/next-12
      - enabled swcMinify
    - Next.js v12.1 https://nextjs.org/blog/next-12-1
    - Next.js v12.2 https://nextjs.org/blog/next-12-2
    - Storybook v6.4 https://storybook.js.org/blog/storybook-6-4/
      - use eslint-plugin-storybook https://github.com/storybookjs/eslint-plugin-storybook
    - Storybook v6.5 https://storybook.js.org/blog/storybook-6-5/
      - dropped emotion alias
    - Webpack https://github.com/webpack/webpack/releases

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+/** @type {import("next").NextConfig} */
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  swcMinify: true,
   // NOTE: ESLintによる型検査を外部で実施しないならば取り除いて型検査を有効化してください
   //       https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
   typescript: { ignoreBuildErrors: true },
