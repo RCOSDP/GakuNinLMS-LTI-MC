c90aaa1531b7c00a4740285e44a7ac29b0b4d292
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-10-27 22:51:04 +0900

    migrate set updateat = createdat

--- server/prisma/migrations/20221025143108_add_lti_resource_link_update_at/migration.sql
+++ server/prisma/migrations/20221025143108_add_lti_resource_link_update_at/migration.sql
@@ -1,2 +1,5 @@
 -- AlterTable
-ALTER TABLE "lti_resource_link" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "lti_resource_link" ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- Set `created_at` to `updated_at`
+UPDATE "lti_resource_link" SET updated_at = created_at;
