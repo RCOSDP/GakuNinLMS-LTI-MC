b17066e717fec142aff3da1fcfe47d8e42e825f0
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-10-31 17:51:52 +0900

    fix migrate update-at not null

--- server/prisma/migrations/20221025143108_add_lti_resource_link_update_at/migration.sql
+++ server/prisma/migrations/20221025143108_add_lti_resource_link_update_at/migration.sql
@@ -1,5 +1,3 @@
--- AlterTable
-ALTER TABLE "lti_resource_link" ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL;
-
--- Set `created_at` to `updated_at`
+ALTER TABLE "lti_resource_link" ADD COLUMN "updated_at" TIMESTAMP(3);
 UPDATE "lti_resource_link" SET updated_at = created_at;
+ALTER TABLE "lti_resource_link" ALTER COLUMN "updated_at" SET NOT NULL;
