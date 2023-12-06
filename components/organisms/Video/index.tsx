dfb40f893a735761336234101bd54d6ca39af998
Youhei Nozaki <youhey.hey0505@gmail.com>
2023-12-06 23:15:39 +0900

    視聴時間を非表示

--- components/organisms/Video/index.tsx
+++ components/organisms/Video/index.tsx
@@ -31,6 +31,7 @@ import getLocaleDateString from "$utils/getLocaleDateString";
 import { authors } from "$utils/descriptionList";
 import TagList from "$molecules/TagList";
 import { useBookmarksByTopicId } from "$utils/bookmark/useBookmarks";
+import { useRouter } from "next/router";
 
 const hidden = css({
   m: 0,
@@ -174,6 +175,7 @@ export default function Video({
   onEnded,
   isPrivateBook = false,
 }: Props) {
+  const router = useRouter();
   const { video, preloadVideo } = useVideoAtom();
   const { book, itemIndex, itemExists } = useBookAtom();
   const { session } = useSessionAtom();
@@ -340,7 +342,7 @@ export default function Video({
         onChange={handleTabIndexChange}
       >
         <Tab label="解説" id="tab-0" aria-controls="panel-0" />
-        {isStudent && (
+        {isStudent && router.query?.bookId && (
           <Tab label="視聴時間詳細" id="tab-1" aria-controls="panel-1" />
         )}
         <Tab
@@ -354,7 +356,7 @@ export default function Video({
           <Markdown>{topic.description}</Markdown>
         </article>
       </TabPanel>
-      {isStudent && (
+      {isStudent && router.query?.bookId && (
         <TabPanel value={tabIndex} index={1} className={timeLineDetail}>
           <SkipButton onClick={handleSkipWatch} />
           <svg height={20} width={BAR_SIZE} xmlns="http://www.w3.org/2000/svg">
