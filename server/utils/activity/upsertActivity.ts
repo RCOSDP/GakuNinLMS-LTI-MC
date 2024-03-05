f5de160d83d3dfdfa298c94facb7b92adfbb1979
Yu Tsuda <yu.tsuda@gmail.com>
2024-03-05 23:05:52 +0900

    wip: feat: ActivityTimeRangeLogを用いた実視聴記録

--- server/utils/activity/upsertActivity.ts
+++ server/utils/activity/upsertActivity.ts
@@ -8,7 +8,12 @@ import type {
 } from "@prisma/client";
 import type { ActivityProps } from "$server/validators/activityProps";
 import type { ActivityTimeRangeProps } from "$server/validators/activityTimeRange";
+import type { ActivityTimeRangeLogProps } from "$server/validators/activityTimeRangeLog";
 import prisma from "$server/utils/prisma";
+
+var NEXT_PUBLIC_ACTIVITY_SEND_INTERVAL2 = Number(
+  process.env.NEXT_PUBLIC_ACTIVITY_SEND_INTERVAL ?? 10
+);
 
 function findActivity({
   learnerId,
@@ -34,6 +39,22 @@ function findActivity({
   });
 }
 
+function findRecentActivityTimeRangeLog(activityId: Activity["id"]) {
+  const date = new Date();
+  date.setSeconds(
+    date.getSeconds() - NEXT_PUBLIC_ACTIVITY_SEND_INTERVAL2 * 1.5
+  );
+  return prisma.activityTimeRangeLog.findMany({
+    where: {
+      activityId: activityId,
+      createdAt: {
+        gte: date,
+      },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
 function toInterval({ startMs, endMs }: ActivityTimeRangeProps) {
   return { low: startMs, high: endMs };
 }
@@ -55,8 +76,62 @@ function merge(
   return timeRanges;
 }
 
+function merge_and_push(
+  self: ActivityTimeRangeLogProps[],
+  other: ActivityTimeRangeProps[]
+): ActivityTimeRangeProps[] {
+  let tmpTimeRanges = [];
+
+  self.forEach((range) => tmpTimeRanges.push(toInterval(range)));
+  tmpTimeRanges = tmpTimeRanges.filter(
+    (element, index, exist) =>
+      exist.findIndex(
+        (e) => e.low === element.low && e.high && element.high
+      ) === index
+  );
+
+  //直近のものを重複排除しつつ、継続視聴の場合は mergeして追記をしないといけない
+  other.forEach((range) => {
+    const interval = toInterval(range);
+
+    // 重複してたら飛ばす
+    let duplicatedTimeRanges = tmpTimeRanges.find(
+      (exist) => exist.low == interval.low && exist.high == interval.high
+    );
+    if (duplicatedTimeRanges) {
+      return;
+    }
+
+    // startMsが同じでかつ、DB上のレコードのendMsが新しく渡されたデータのendMsより小さい場合、視聴中を表すレコードとしてDB上のレコードを省く
+    tmpTimeRanges = tmpTimeRanges.filter(
+      (exist) => !(exist.low == interval.low && exist.high < interval.high)
+    );
+    tmpTimeRanges.push(interval);
+  });
+
+  const timeRanges = tmpTimeRanges.map(
+    ({ low, high, created_at, updated_at }) => ({
+      startMs: low,
+      endMs: high,
+      createdAt: created_at,
+      updatedAt: updated_at,
+    })
+  );
+
+  return timeRanges;
+}
+
 function cleanup(activityId: Activity["id"]) {
   return prisma.activityTimeRange.deleteMany({ where: { activityId } });
+}
+
+function cleanupRecentTimeRangeLogs(
+  timeRangeLogs: ActivityTimeRangeLogProps[]
+) {
+  const ids = timeRangeLogs.map((log) => {
+    return log.id;
+  });
+  return prisma.activityTimeRangeLog.deleteMany({ where: { id: { in: ids } } });
 }
 
 function upsert({
@@ -65,12 +140,14 @@ function upsert({
   ltiConsumerId,
   ltiContextId,
   timeRanges,
+  timeRangeLogs,
 }: {
   learnerId: User["id"];
   topicId: Topic["id"];
   ltiConsumerId: LtiConsumer["id"];
   ltiContextId: LtiContext["id"];
   timeRanges: ActivityTimeRangeProps[];
+  timeRangeLogs: ActivityTimeRangeLogProps[];
 }) {
   const totalTimeMs = timeRanges.reduce(
     (a, { startMs, endMs }) => a + endMs - startMs,
@@ -79,6 +156,7 @@ function upsert({
   const input = {
     totalTimeMs,
     timeRanges: { create: timeRanges },
+    timeRangeLogs: { create: timeRangeLogs },
   };
   return prisma.activity.upsert({
     where: {
@@ -122,11 +200,26 @@ async function upsertActivity({
     ltiConsumerId,
     ltiContextId,
   });
+
+  const recentTimeRangeLogs = await findRecentActivityTimeRangeLog(exists?.id);
+
   const timeRanges = merge(exists?.timeRanges ?? [], activity.timeRanges);
+  const timeRangeLogs = merge_and_push(
+    recentTimeRangeLogs,
+    activity.timeRanges
+  );
 
   await prisma.$transaction([
+    ...(exists ? [cleanupRecentTimeRangeLogs(recentTimeRangeLogs)] : []),
     ...(exists ? [cleanup(exists.id)] : []),
-    upsert({ learnerId, topicId, ltiConsumerId, ltiContextId, timeRanges }),
+    upsert({
+      learnerId,
+      topicId,
+      ltiConsumerId,
+      ltiContextId,
+      timeRanges,
+      timeRangeLogs,
+    }),
   ]);
 
   return { timeRanges };
