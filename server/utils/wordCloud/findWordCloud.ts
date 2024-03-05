4b94601375e6ec85421fffd99548e645f79ff5ed
Youhei Nozaki <youhey.hey0505@gmail.com>
2024-03-06 03:01:49 +0900

    install kuromoji

--- server/utils/wordCloud/findWordCloud.ts
+++ server/utils/wordCloud/findWordCloud.ts
@@ -1,8 +1,31 @@
 import prisma from "$server/utils/prisma";
+import kuromoji from "kuromoji";
 import type { WordCloudParams } from "$server/validators/wordCloudParams";
 import type { WordCloudSchema } from "$server/models/wordCloud";
 import { getBookIncludingArg } from "$server/utils/book/bookToBookSchema";
 import findBookmarks from "../bookmark/findBookmarks";
+
+function buildTokenizer() {
+  return new Promise<kuromoji.Tokenizer<kuromoji.IpadicFeatures>>(
+    (resolve, reject) => {
+      kuromoji
+        .builder({ dicPath: "node_modules/kuromoji/dict" })
+        .build((err, tokenizer) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(tokenizer);
+          }
+        });
+    }
+  );
+}
+
+const tokenize = async (text: string) => {
+  const tokenizer = await buildTokenizer();
+
+  return tokenizer.tokenize(text);
+};
 
 async function findWordCloud(
   bookId: WordCloudParams["bookId"],
@@ -32,6 +55,8 @@ async function findWordCloud(
         continue;
       }
       bookmarkMemoContents.push(bookmark.memoContent);
+      const res = await tokenize(bookmark.memoContent);
+      console.log(res);
     }
   }
   console.log(bookmarkMemoContents);
