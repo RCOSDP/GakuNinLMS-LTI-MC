b83d67de24d49123b9d6cb63d77fff673e4c081d
Yu Tsuda <yu.tsuda@gmail.com>
2024-10-20 23:59:08 +0900

    feat: 学習分析「トピック」タブで平均視聴時間（秒）を平均学習完了率に変更

--- components/templates/Dashboard.tsx
+++ components/templates/Dashboard.tsx
@@ -181,8 +181,8 @@ export default function Dashboard(props: Props) {
 
   const { data: rewatchRates } = useRewatchRate(
     scope === "current-lti-context-only"
-　);
-  
+  );
+
   const activitiesByBooksAndTopics = useMemo(
     () =>
       getActivitiesByBooksAndTopics({
@@ -304,7 +304,7 @@ export default function Dashboard(props: Props) {
           <div className={classes.topicLabel}>
             <div className={classes.topicTitleColumn}></div>
             <div className={classes.topicColumn}>動画の長さ（秒）</div>
-            <div className={classes.topicColumn}>平均視聴時間（秒）</div>
+            <div className={classes.topicColumn}>平均学習完了率</div>
             <div className={classes.topicColumn}>平均繰返視聴割合</div>
           </div>
           {activitiesByBooksAndTopics.map(
