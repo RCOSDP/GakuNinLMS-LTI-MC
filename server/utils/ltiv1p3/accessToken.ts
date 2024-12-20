312d08668c7c515c045bb58e16fbac2eed303cbd
Youhei Nozaki <youhey.hey0505@gmail.com>
2024-12-20 11:50:02 +0900

    add result.readonly to scope

--- server/utils/ltiv1p3/accessToken.ts
+++ server/utils/ltiv1p3/accessToken.ts
@@ -7,8 +7,9 @@ async function grant(client: Client): Promise<string> {
       grant_type: "client_credentials",
       scope: [
         "https://purl.imsglobal.org/spec/lti-ags/scope/score",
-        "https://purl.imsglobal.org/spec/lti-nrps/scope/contextmembership.readonly",
         "https://purl.imsglobal.org/spec/lti-ags/scope/lineitem.readonly",
+        "https://purl.imsglobal.org/spec/lti-ags/scope/result.readonly",
+        "https://purl.imsglobal.org/spec/lti-nrps/scope/contextmembership.readonly",
       ].join(" "),
     });
     return tokens.access_token ?? "";
