f6381b9546dcde201c76b2607a4d1d47285774c7
Yu Tsuda <yu.tsuda@gmail.com>
2025-02-27 22:31:05 +0900

    feat: ダウンロードページでは受講者の同期やコース毎の分析は不要

--- components/templates/Download.tsx
+++ components/templates/Download.tsx
@@ -1,23 +1,14 @@
-import { useMemo, useCallback } from "react";
+import { useCallback } from "react";
 import { Box, Card, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import GetAppOutlinedIcon from "@mui/icons-material/GetAppOutlined";
-import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
 import makeStyles from "@mui/styles/makeStyles";
 import Container from "$atoms/Container";
-import ActionHeader from "$organisms/ActionHeader";
-import type { ActivityScope } from "$types/activityScope";
-import ActivityScopeSelect from "$molecules/ActivityScopeSelect";
 import useCardStyles from "$styles/card";
 import type { BookActivitySchema } from "$server/models/bookActivity";
 import type { SessionSchema } from "$server/models/session";
 import { gray } from "$theme/colors";
 import downloadBookActivity from "$utils/bookLearningActivity/download";
-import useDialogProps from "$utils/useDialogProps";
-import useMemberships from "$utils/useMemberships";
-import MembersDialog from "$organisms/MembersDialog";
-import useLtiMembersHandler from "$utils/useLtiMembersHandler";
-import type { LtiNrpsContextMemberSchema } from "$server/models/ltiNrpsContextMember";
 
 const useStyles = makeStyles(() => ({
   card: {
@@ -33,74 +24,21 @@ const useStyles = makeStyles(() => ({
 type Props = {
   session: SessionSchema;
   bookActivities: Array<BookActivitySchema>;
-  scope: ActivityScope;
-  onScopeChange(activityScope: ActivityScope): void;
 };
 
 export default function Download(props: Props) {
-  const { session, bookActivities, scope } = props;
-  const { data: memberships } = useMemberships();
-  const newLtiMembers = useMemo(() => {
-    if (!memberships) {
-      return [];
-    }
-    const { members, currentLtiMembers } = memberships;
-    return members.filter((member) => {
-      return currentLtiMembers?.every(
-        (learner) => learner.userId !== member.user_id
-      );
-    });
-  }, [memberships]);
-  const updateLtiMembers = useLtiMembersHandler();
+  const { session, bookActivities } = props;
   const classes = useStyles();
   const cardClasses = useCardStyles();
   const handleBookActivityDownloadClick = useCallback(() => {
     void downloadBookActivity(bookActivities, "視聴分析データ.csv", session);
   }, [bookActivities, session]);
-  const {
-    data: membersData,
-    dispatch: membersDispatch,
-    ...membersDialogProps
-  } = useDialogProps<{
-    members: LtiNrpsContextMemberSchema[];
-  }>();
-
-  const handleUpdateLtiMembers = useCallback(
-    async (members: LtiNrpsContextMemberSchema[]) => {
-      await updateLtiMembers({
-        members,
-        currentLtiContextOnly: scope === "current-lti-context-only",
-      });
-      membersDialogProps.onClose();
-    },
-    [membersDialogProps, scope, updateLtiMembers]
-  );
-  const handleMembershipClick = useCallback(
-    (members: LtiNrpsContextMemberSchema[]) => () =>
-      membersDispatch({ members }),
-    [membersDispatch]
-  );
 
   return (
     <Container maxWidth="md">
       <Typography sx={{ mt: 5 }} variant="h4">
         ダウンロード
       </Typography>
-      <ActionHeader>
-        <ActivityScopeSelect
-          value={props.scope}
-          onActivityScopeChange={props.onScopeChange}
-        />
-        <Button
-          onClick={handleMembershipClick(memberships?.members || [])}
-          color="primary"
-          variant="contained"
-          size="small"
-        >
-          <GroupOutlinedIcon fontSize="small" />
-          受講者の同期
-        </Button>
-      </ActionHeader>
       <Card classes={cardClasses} className={classes.card}>
         <Box className={classes.body}>
           <Button
@@ -116,14 +54,6 @@ export default function Download(props: Props) {
           </Button>
         </Box>
       </Card>
-      {membersData && (
-        <MembersDialog
-          members={membersData.members}
-          newLtiMembers={newLtiMembers}
-          handleUpdateLtiMembers={handleUpdateLtiMembers}
-          {...membersDialogProps}
-        />
-      )}
     </Container>
   );
 }
--- pages/download.tsx
+++ pages/download.tsx
@@ -1,33 +1,20 @@
-import { useState } from "react";
-import type { ActivityScope } from "$types/activityScope";
 import Download from "$templates/Download";
 import Placeholder from "$templates/Placeholder";
 import Problem from "$organisms/Problem";
 import { useSessionAtom } from "$store/session";
 import useActivity from "$utils/useActivity";
-import { NEXT_PUBLIC_ACTIVITY_LTI_CONTEXT_ONLY } from "$utils/env";
 import { isAdministrator } from "$utils/session";
 
 function Index() {
   const { session } = useSessionAtom();
-  const [scope, setScope] = useState<ActivityScope>(
-    NEXT_PUBLIC_ACTIVITY_LTI_CONTEXT_ONLY ? "current-lti-context-only" : "topic"
-  );
-  const { data, error } = useActivity(scope === "current-lti-context-only");
+  const { data, error } = useActivity(true);
 
   if (error) return <Problem title="学習分析データの取得に失敗しました" />;
   if (!session || !isAdministrator(session))
     return <Problem title="管理者のみアクセス可能です" />;
   if (!data) return <Placeholder />;
 
-  return (
-    <Download
-      session={session}
-      scope={scope}
-      onScopeChange={setScope}
-      {...data}
-    />
-  );
+  return <Download session={session} {...data} />;
 }
 
 export default Index;
