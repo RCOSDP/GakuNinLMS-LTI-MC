e83fe1c9872977363e42a68878bf637cf9cdf44b
Yu Tsuda <yu.tsuda@gmail.com>
2025-03-20 22:20:05 +0900

    chore: Object.groupBy (ES2024) は lint で無視

--- components/organisms/ActivityRewatchGraph.tsx
+++ components/organisms/ActivityRewatchGraph.tsx
@@ -247,7 +247,9 @@ export default function ActivityRewatchGraph(props: Props) {
       .filter((c) => c.count <= NEXT_PUBLIC_REWATCH_GRAPH_COUNT_THRESHOLD) ||
     [];
 
-  const plotEachStartMs = Object.groupBy(plot, (p: PlotSchema) => p.startMs); // ES2024
+  // ES2024: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/groupBy
+  // eslint-disable-next-line
+  const plotEachStartMs = Object.groupBy(plot, (p: PlotSchema) => p.startMs);
   const average: PlotSchema[] = [];
   for (const key of Object.keys(plotEachStartMs)) {
     const startMs = key;
